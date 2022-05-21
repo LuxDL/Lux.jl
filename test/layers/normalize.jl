1ff0363fa6f75bb60933c60b8802e591d2919585
Avik Pal <avikpal@mit.edu>
2022-05-21 10:51:40 -0400

    Oops naming

--- test/layers/normalize.jl
+++ test/layers/normalize.jl
@@ -5,93 +5,93 @@ include("../utils.jl")
 rng = Random.default_rng()
 Random.seed!(rng, 0)
 
-# @testset "BatchNorm" begin
-#     let m = BatchNorm(2), x = [
-#             1.0f0 3.0f0 5.0f0
-#             2.0f0 4.0f0 6.0f0
-#         ]
-#         ps, st = Lux.setup(rng, m)
+@testset "BatchNorm" begin
+    let m = BatchNorm(2), x = [
+            1.0f0 3.0f0 5.0f0
+            2.0f0 4.0f0 6.0f0
+        ]
+        ps, st = Lux.setup(rng, m)
 
-#         @test Lux.parameterlength(m) == 2 * 2
+        @test Lux.parameterlength(m) == 2 * 2
 
-#         @test ps.bias == [0, 0]  # init_bias(2)
-#         @test ps.scale == [1, 1]  # init_scale(2)
+        @test ps.bias == [0, 0]  # init_bias(2)
+        @test ps.scale == [1, 1]  # init_scale(2)
 
-#         y, st_ = pullback(m, x, ps, st)[1]
-#         @test isapprox(y, [-1.22474 0 1.22474; -1.22474 0 1.22474], atol=1.0e-5)
-#         # julia> x
-#         #  2×3 Array{Float64,2}:
-#         #  1.0  3.0  5.0
-#         #  2.0  4.0  6.0
-#         #
-#         # mean of batch will be
-#         #  (1. + 3. + 5.) / 3 = 3
-#         #  (2. + 4. + 6.) / 3 = 4
-#         #
-#         # ∴ update rule with momentum:
-#         #  .1 * 3 + 0 = .3
-#         #  .1 * 4 + 0 = .4
-#         @test st_.running_mean ≈ reshape([0.3, 0.4], 2, 1)
+        y, st_ = pullback(m, x, ps, st)[1]
+        @test isapprox(y, [-1.22474 0 1.22474; -1.22474 0 1.22474], atol=1.0e-5)
+        # julia> x
+        #  2×3 Array{Float64,2}:
+        #  1.0  3.0  5.0
+        #  2.0  4.0  6.0
+        #
+        # mean of batch will be
+        #  (1. + 3. + 5.) / 3 = 3
+        #  (2. + 4. + 6.) / 3 = 4
+        #
+        # ∴ update rule with momentum:
+        #  .1 * 3 + 0 = .3
+        #  .1 * 4 + 0 = .4
+        @test st_.running_mean ≈ reshape([0.3, 0.4], 2, 1)
 
-#         # julia> .1 .* var(x, dims = 2, corrected=false) .* (3 / 2).+ .9 .* [1., 1.]
-#         # 2×1 Array{Float64,2}:
-#         #  1.3
-#         #  1.3
-#         @test st_.running_var ≈ 0.1 .* var(x; dims=2, corrected=false) .* (3 / 2) .+ 0.9 .* [1.0, 1.0]
+        # julia> .1 .* var(x, dims = 2, corrected=false) .* (3 / 2).+ .9 .* [1., 1.]
+        # 2×1 Array{Float64,2}:
+        #  1.3
+        #  1.3
+        @test st_.running_var ≈ 0.1 .* var(x; dims=2, corrected=false) .* (3 / 2) .+ 0.9 .* [1.0, 1.0]
 
-#         st_ = Lux.testmode(st_)
-#         x′ = m(x, ps, st_)[1]
-#         @test isapprox(x′[1], (1 .- 0.3) / sqrt(1.3), atol=1.0e-5)
+        st_ = Lux.testmode(st_)
+        x′ = m(x, ps, st_)[1]
+        @test isapprox(x′[1], (1 .- 0.3) / sqrt(1.3), atol=1.0e-5)
 
-#         @inferred m(x, ps, st)
+        @inferred m(x, ps, st)
 
-#         @test_call m(x, ps, st)
-#         @test_opt target_modules = (Lux,) m(x, ps, st)
+        @test_call m(x, ps, st)
+        @test_opt target_modules = (Lux,) m(x, ps, st)
 
-#         test_gradient_correctness_fdm((x, ps) -> sum(first(m(x, ps, st))), x, ps; atol=1.0f-3, rtol=1.0f-3)
-#     end
+        test_gradient_correctness_fdm((x, ps) -> sum(first(m(x, ps, st))), x, ps; atol=1.0f-3, rtol=1.0f-3)
+    end
 
-#     let m = BatchNorm(2; track_stats=false), x = [1.0f0 3.0f0 5.0f0; 2.0f0 4.0f0 6.0f0]
-#         ps, st = Lux.setup(rng, m)
-#         @inferred m(x, ps, st)
-#         @test_call m(x, ps, st)
-#         @test_opt target_modules = (Lux,) m(x, ps, st)
+    let m = BatchNorm(2; track_stats=false), x = [1.0f0 3.0f0 5.0f0; 2.0f0 4.0f0 6.0f0]
+        ps, st = Lux.setup(rng, m)
+        @inferred m(x, ps, st)
+        @test_call m(x, ps, st)
+        @test_opt target_modules = (Lux,) m(x, ps, st)
 
-#         test_gradient_correctness_fdm((x, ps) -> sum(first(m(x, ps, st))), x, ps; atol=1.0f-3, rtol=1.0f-3)
-#     end
+        test_gradient_correctness_fdm((x, ps) -> sum(first(m(x, ps, st))), x, ps; atol=1.0f-3, rtol=1.0f-3)
+    end
 
-#     # with activation function
-#     let m = BatchNorm(2, sigmoid), x = [
-#             1.0f0 3.0f0 5.0f0
-#             2.0f0 4.0f0 6.0f0
-#         ]
-#         ps, st = Lux.setup(rng, m)
-#         st = Lux.testmode(st)
-#         y, st_ = m(x, ps, st)
-#         @test isapprox(y, sigmoid.((x .- st_.running_mean) ./ sqrt.(st_.running_var .+ m.epsilon)), atol=1.0e-7)
-#         @inferred m(x, ps, st)
-#         @test_call m(x, ps, st)
-#         @test_opt target_modules = (Lux,) m(x, ps, st)
+    # with activation function
+    let m = BatchNorm(2, sigmoid), x = [
+            1.0f0 3.0f0 5.0f0
+            2.0f0 4.0f0 6.0f0
+        ]
+        ps, st = Lux.setup(rng, m)
+        st = Lux.testmode(st)
+        y, st_ = m(x, ps, st)
+        @test isapprox(y, sigmoid.((x .- st_.running_mean) ./ sqrt.(st_.running_var .+ m.epsilon)), atol=1.0e-7)
+        @inferred m(x, ps, st)
+        @test_call m(x, ps, st)
+        @test_opt target_modules = (Lux,) m(x, ps, st)
 
-#         test_gradient_correctness_fdm((x, ps) -> sum(first(m(x, ps, st))), x, ps; atol=1.0f-3, rtol=1.0f-3)
-#     end
+        test_gradient_correctness_fdm((x, ps) -> sum(first(m(x, ps, st))), x, ps; atol=1.0f-3, rtol=1.0f-3)
+    end
 
-#     let m = BatchNorm(2), x = reshape(Float32.(1:6), 3, 2, 1)
-#         ps, st = Lux.setup(rng, m)
-#         st = Lux.trainmode(st)
-#         @test_throws AssertionError m(x, ps, st)[1]
-#     end
+    let m = BatchNorm(2), x = reshape(Float32.(1:6), 3, 2, 1)
+        ps, st = Lux.setup(rng, m)
+        st = Lux.trainmode(st)
+        @test_throws AssertionError m(x, ps, st)[1]
+    end
 
-#     let m = BatchNorm(32), x = randn(Float32, 416, 416, 32, 1)
-#         ps, st = Lux.setup(rng, m)
-#         st = Lux.testmode(st)
-#         m(x, ps, st)
-#         @test (@allocated m(x, ps, st)) < 100_000_000
-#         @inferred m(x, ps, st)
-#         @test_call m(x, ps, st)
-#         @test_opt target_modules = (Lux,) m(x, ps, st)
-#     end
-# end
+    let m = BatchNorm(32), x = randn(Float32, 416, 416, 32, 1)
+        ps, st = Lux.setup(rng, m)
+        st = Lux.testmode(st)
+        m(x, ps, st)
+        @test (@allocated m(x, ps, st)) < 100_000_000
+        @inferred m(x, ps, st)
+        @test_call m(x, ps, st)
+        @test_opt target_modules = (Lux,) m(x, ps, st)
+    end
+end
 
 @testset "GroupNorm" begin
     # begin tests
