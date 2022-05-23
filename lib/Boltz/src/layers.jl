560c7eed0df3e3d14a1fd482c9de7636da0036db
Avik Pal <avikpal@mit.edu>
2022-05-23 13:11:57 -0400

    fix activation

--- lib/Boltz/src/layers.jl
+++ lib/Boltz/src/layers.jl
@@ -1,7 +1,7 @@
 function layer_norm(sz::Integer, activation=identity; affine::Bool=true, epsilon::Real=1f-5)
     if affine
         return Chain(
-            WrappedFunction(x -> normalise(x, activation; dims=1, epsilon=epsilon)),
+            WrappedFunction(x -> normalise(x; dims=1, epsilon=epsilon)),
             Scale(sz, activation),
         )
     else
