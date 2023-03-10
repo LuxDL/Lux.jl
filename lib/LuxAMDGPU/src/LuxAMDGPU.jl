ea12dec40e39898439e0c47ef892602c317df9ca
Avik Pal <avikpal@mit.edu>
2023-03-09 19:05:27 -0500

    Move more stuff to Lux accelerator triggers

--- lib/LuxAMDGPU/src/LuxAMDGPU.jl
+++ lib/LuxAMDGPU/src/LuxAMDGPU.jl
@@ -2,7 +2,37 @@ module LuxAMDGPU
 
 using Reexport
 
-@reexport using AMDGPU
-@reexport using ROCKernels
+@reexport using AMDGPU, NNlib, ROCKernels
+
+const USE_AMDGPU = Ref{Union{Nothing, Bool}}(nothing)
+
+function check_use_amdgpu!()
+    USE_AMDGPU[] === nothing || return
+
+    USE_AMDGPU[] = AMDGPU.functional()
+    if USE_AMDGPU[]
+        if !AMDGPU.functional(:MIOpen)
+            @warn """
+            MIOpen is not functional in AMDGPU.jl, some functionality will not be available.
+            """ maxlog=1
+        end
+    else
+        @info """
+        The AMDGPU function is being called but the AMDGPU is not functional.
+        Defaulting back to the CPU. (No action is required if you want to run on the CPU).
+        """ maxlog=1
+    end
+    return
+end
+
+"""
+    functional()
+
+Check if LuxAMDGPU is functional.
+"""
+function functional()::Bool
+    check_use_amdgpu!()
+    return USE_AMDGPU[]
+end
 
 end
--- lib/LuxCUDA/src/LuxCUDA.jl
+++ lib/LuxCUDA/src/LuxCUDA.jl
@@ -2,9 +2,36 @@ module LuxCUDA
 
 using Reexport
 
-@reexport using CUDA
-@reexport using CUDAKernels
-@reexport using NNlibCUDA
-@reexport using cuDNN
+@reexport using CUDA, CUDAKernels, NNlibCUDA, cuDNN
+
+const USE_CUDA = Ref{Union{Nothing, Bool}}(nothing)
+
+function check_use_cuda!()
+    USE_CUDA[] === nothing || return
+
+    USE_CUDA[] = CUDA.functional()
+    if USE_CUDA[]
+        if !cuDNN.has_cudnn()
+            @warn """
+            CUDA.jl found cuda, but did not find libcudnn. Some functionality will not be
+            available.""" maxlog=1
+        end
+    else
+        @info """
+        The CUDA GPU function is being called but the GPU is not accessible.
+        Defaulting back to the CPU. (No action is required if you want to run on the CPU).
+        """ maxlog=1
+    end
+end
+
+"""
+    functional()
+
+Check if LuxCUDA is functional.
+"""
+function functional()::Bool
+    check_use_cuda!()
+    return USE_CUDA[]
+end
 
 end
