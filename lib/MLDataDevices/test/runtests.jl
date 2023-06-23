844d4fe15cba7f269bad4b4ccd3f2cf1baebfd8e
Avik Pal <avikpal@mit.edu>
2023-06-23 13:17:14 -0400

    Allow testing on <1.9

--- lib/MLDataDevices/test/runtests.jl
+++ lib/MLDataDevices/test/runtests.jl
@@ -1,12 +1,19 @@
 using SafeTestsets, Test
 using LuxCore, LuxDeviceUtils
 
+@static if VERSION ≥ v"1.9"
+    using Pkg
+    Pkg.add("LuxAMDGPU")
+end
+
 @testset "LuxDeviceUtils Tests" begin
     @safetestset "LuxCUDA" begin
         include("luxcuda.jl")
     end
 
-    @safetestset "LuxAMDGPU" begin
-        include("luxamdgpu.jl")
+    @static if VERSION ≥ v"1.9"
+        @safetestset "LuxAMDGPU" begin
+            include("luxamdgpu.jl")
+        end
     end
 end
