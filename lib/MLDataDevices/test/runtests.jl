b462b054935828a913eefcd98b631f28c8487b30
Avik Pal <avikpal@mit.edu>
2023-06-23 12:35:24 -0400

    Add CI

--- lib/MLDataDevices/test/runtests.jl
+++ lib/MLDataDevices/test/runtests.jl
@@ -1,4 +1,8 @@
 using Test
-using LuxCore, LuxDeviceUtils, LuxAMDGPU, LuxCUDA
+using LuxCore, LuxDeviceUtils
+using LuxAMDGPU, LuxCUDA  # Accelerators
+using FillArrays, Zygote  # Extensions
 
-@testset "LuxDeviceUtils Tests" begin end
+@testset "LuxDeviceUtils Tests" begin
+    @test 1 + 1 == 2
+end
