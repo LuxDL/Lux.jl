fefbb23069e1d65cecbcaf9e0d875b9cf29fff0c
Avik Pal <avikpal@mit.edu>
2023-06-30 11:48:39 -0400

    Add Aqua tests

--- lib/LuxLib/test/runtests.jl
+++ lib/LuxLib/test/runtests.jl
@@ -5,20 +5,26 @@ using SafeTestsets, Test
         include("api/dropout.jl")
     end
 
-    @time @safetestset "BatchNorm" begin
-        include("api/batchnorm.jl")
-    end
-    @time @safetestset "GroupNorm" begin
-        include("api/groupnorm.jl")
-    end
-    @time @safetestset "InstanceNorm" begin
-        include("api/instancenorm.jl")
-    end
-    @time @safetestset "LayerNorm" begin
-        include("api/layernorm.jl")
+    @testset "Normalization" begin
+        @time @safetestset "BatchNorm" begin
+            include("api/batchnorm.jl")
+        end
+        @time @safetestset "GroupNorm" begin
+            include("api/groupnorm.jl")
+        end
+        @time @safetestset "InstanceNorm" begin
+            include("api/instancenorm.jl")
+        end
+        @time @safetestset "LayerNorm" begin
+            include("api/layernorm.jl")
+        end
     end
 
     @time @safetestset "ForwardDiff Extension" begin
         include("ext/LuxLibForwardDiffExt.jl")
+    end
+
+    @time @safetestset "Aqua Tests" begin
+        include("aqua.jl")
     end
 end
