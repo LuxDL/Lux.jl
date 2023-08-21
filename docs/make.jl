a74c986ca417e0a848c22394c66c18e354ee2d38
Avik Pal <avikpal@mit.edu>
2023-08-21 06:59:55 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -15,5 +15,5 @@ makedocs(; sitename="Lux", authors="Avik Pal et al.", clean=true, doctest=true,
     draft=false, strict=[:doctest, :linkcheck, :parse_error, :example_block, :missing_docs],
     source="src", build=joinpath(@__DIR__, "build"))
 
-deploydocs(; repo="github.com/LuxDL/Lux.jl.git", push_preview=true,
-    target="page/.vitepress/dist", devbranch="main")
+deploydocs(; repo="github.com/LuxDL/Lux.jl.git", push_preview=true, target="build",
+    devbranch="main")
