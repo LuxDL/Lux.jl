a3378389bb732d77efec5a102727c524ee03d835
Avik Pal <avikpal@mit.edu>
2023-08-26 15:46:23 -0400

    Use separate processes to generate the tutorials

--- docs/tutorials.jl
+++ docs/tutorials.jl
@@ -3,12 +3,6 @@ using Literate
 get_example_path(p) = joinpath(@__DIR__, "..", "examples", p)
 OUTPUT = joinpath(@__DIR__, "src", "tutorials")
 
-function preprocess(path, str)
-    dir = dirname(path)
-    str = replace(str, "__DIR = @__DIR__" => "__DIR = \"$dir\"")
-    return str
-end
-
 BEGINNER_TUTORIALS = ["Basics/main.jl", "PolynomialFitting/main.jl", "SimpleRNN/main.jl"]
 INTERMEDIATE_TUTORIALS = ["NeuralODE/main.jl", "BayesianNN/main.jl", "HyperNet/main.jl"]
 ADVANCED_TUTORIALS = ["GravitationalWaveForm/main.jl"]
@@ -19,7 +13,8 @@ withenv("JULIA_DEBUG" => "Literate") do
             ("advanced", ADVANCED_TUTORIALS)), (i, p) in enumerate(paths)
         name = "$(i)_$(first(rsplit(p, "/")))"
         p_ = get_example_path(p)
-        Literate.markdown(p_, joinpath(OUTPUT, d); execute=true, name, documenter=true,
-            preprocess=Base.Fix1(preprocess, p_))
+        jl_expr = "using Literate; preprocess(path, str) = replace(str, \"__DIR = @__DIR__\" => \"__DIR = \\\"\$(dirname(path))\\\"\"); Literate.markdown(\"$(p_)\", \"$(joinpath(OUTPUT, d))\"; execute=true, name=\"$name\", documenter=true, preprocess=Base.Fix1(preprocess, \"$(p_)\"))"
+        cm = `julia --project=$(@__DIR__) -e $(jl_expr)`
+        run(cm)
     end
 end
