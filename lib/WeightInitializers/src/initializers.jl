1f2796a4ce21201ab489553e8c9168bb018a7215
MartinuzziFrancesco <martinuzzi.francesco@gmail.com>
2024-01-20 18:21:55 +0100

    fixing orthogonal

--- lib/WeightInitializers/src/initializers.jl
+++ lib/WeightInitializers/src/initializers.jl
@@ -143,17 +143,29 @@ Cannot construct a vector, i.e., `length(dims) == 1` is forbidden.
 
 [^Saxe14] Saxe, McClelland, Ganguli. "Exact solutions to the nonlinear dynamics of learning in deep linear neural networks", ICLR 2014, https://arxiv.org/abs/1312.6120
 """
-function orthogonal(rng::AbstractRNG, ::Type{T}, dims::Integer...;
-        gain::Number=1) where {T <: Real}
+function orthogonal(rng::AbstractRNG, ::Type{T}, dims::Integer...; gain::Number=T(1)) where {T <: Real}
     @assert length(dims) > 1 "Creating vectors (length(dims) == 1) is not allowed"
-    rows, cols = dims
+    
+    if length(dims) == 2
+        rows, cols = dims
+    else
+        rows = prod(dims[1:end-1])
+        cols = dims[end]
+    end
+
     if rows < cols
         return permutedims(orthogonal(rng, T, cols, rows; gain))
     end
+    
     mat = randn(rng, T, rows, cols)
     Q, R = LinearAlgebra.qr(mat)
     mat .= Array(Q) * sign.(LinearAlgebra.Diagonal(R)) .* T(gain)
-    return mat
+
+    if length(dims) > 2
+        return reshape(mat, dims)
+    else
+        return mat
+    end
 end
 
 # Default Fallbacks for all functions
