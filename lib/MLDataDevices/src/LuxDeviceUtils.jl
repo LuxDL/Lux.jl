b5b0f21de81467aa36961da002b3d5bec8062e30
Avik Pal <avik.pal.2017@gmail.com>
2024-02-05 11:48:19 -0500

    Update src/LuxDeviceUtils.jl

--- lib/MLDataDevices/src/LuxDeviceUtils.jl
+++ lib/MLDataDevices/src/LuxDeviceUtils.jl
@@ -243,7 +243,7 @@ for (dev) in (:CPU, :CUDA, :AMDGPU, :Metal)
             return fmap(Base.Fix1(adapt, $(ladaptor)()), x; exclude=_isleaf)
         end
         function (::$(ldev))(NN::LuxCore.AbstractExplicitLayer)
-            @warn "Lux layers are stateless and hence don't participate in device transfers. Apply this function on the parameters and states generated using `Lux.setup`." maxlog = 1
+            @warn "Lux layers are stateless and hence don't participate in device transfers. Apply this function on the parameters and states generated using `Lux.setup`." maxlog=1
             return NN
         end
     end
