830c786db105cfc6e14331929b26aa43d22e1bc9
avik-pal <avik-pal@users.noreply.github.com>
2024-02-11 00:52:25 +0000

    Format .jl files

--- lib/WeightInitializers/src/WeightInitializers.jl
+++ lib/WeightInitializers/src/WeightInitializers.jl
@@ -11,9 +11,9 @@ include("utils.jl")
 include("initializers.jl")
 
 export zeros64, ones64, rand64, randn64, zeros32, ones32, rand32, randn32, zeros16, ones16,
-    rand16, randn16
+       rand16, randn16
 export zerosC64, onesC64, randC64, randnC64, zerosC32, onesC32, randC32, randnC32, zerosC16,
-    onesC16, randC16, randnC16
+       onesC16, randC16, randnC16
 export glorot_normal, glorot_uniform
 export kaiming_normal, kaiming_uniform
 export truncated_normal
