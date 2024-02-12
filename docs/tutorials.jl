db891f60e19b024da1bd56c91dc40197e7daebfc
Avik Pal <avikpal@mit.edu>
2024-02-12 10:53:24 -0500

    Distributed build for Lux Tutorials

--- docs/tutorials.jl
+++ docs/tutorials.jl
@@ -1,29 +1,34 @@
-using Literate
+using Distributed
 
-preprocess(path, str) = replace(str, "__DIR = @__DIR__" => "__DIR = \"$(dirname(path))\"")
+const LUX_DOCUMENTATION_NWORKERS = parse(Int, get(ENV, "LUX_DOCUMENTATION_NWORKERS", "1"))
+@info "Lux Tutorial Build Running tutorials with $(LUX_DOCUMENTATION_NWORKERS) workers."
+addprocs(LUX_DOCUMENTATION_NWORKERS)
 
-get_example_path(p) = joinpath(@__DIR__, "..", "examples", p)
+@everywhere using Literate
+
+@everywhere function preprocess(path, str)
+    return replace(str, "__DIR = @__DIR__" => "__DIR = \"$(dirname(path))\"")
+end
+
+@everywhere get_example_path(p) = joinpath(@__DIR__, "..", "examples", p)
+
 OUTPUT = joinpath(@__DIR__, "src", "tutorials")
 
 BEGINNER_TUTORIALS = ["Basics/main.jl", "PolynomialFitting/main.jl", "SimpleRNN/main.jl"]
 INTERMEDIATE_TUTORIALS = ["NeuralODE/main.jl", "BayesianNN/main.jl", "HyperNet/main.jl"]
 ADVANCED_TUTORIALS = ["GravitationalWaveForm/main.jl"]
 
-withenv("JULIA_DEBUG" => "Literate") do
-    # tasks = []
-    for (d, paths) in (("beginner", BEGINNER_TUTORIALS),
-            ("intermediate", INTERMEDIATE_TUTORIALS),
-            ("advanced", ADVANCED_TUTORIALS)), (i, p) in enumerate(paths)
+TUTORIALS = [collect(Iterators.product(["beginner"], BEGINNER_TUTORIALS))...,
+    collect(Iterators.product(["intermediate"], INTERMEDIATE_TUTORIALS))...,
+    collect(Iterators.product(["advanced"], ADVANCED_TUTORIALS))...]
+
+pmap(enumerate(TUTORIALS)) do (i, (d, p))
+    println("Running tutorial $(i): $(p) on worker $(myid())")
+    withenv("JULIA_DEBUG" => "Literate") do
         name = "$(i)_$(first(rsplit(p, "/")))"
         p_ = get_example_path(p)
-        # Literate.markdown(p_, joinpath(OUTPUT, d); execute=true, name=name, documenter=true,
-        #     preprocess=Base.Fix1(preprocess, p_))
-        # Using `@spawn` causes non-deterministic failures
-        jl_expr = "using Literate; preprocess(path, str) = replace(str, \"__DIR = @__DIR__\" => \"__DIR = \\\"\$(dirname(path))\\\"\"); Literate.markdown(\"$(p_)\", \"$(joinpath(OUTPUT, d))\"; execute=true, name=\"$name\", documenter=true, preprocess=Base.Fix1(preprocess, \"$(p_)\"))"
-        cm = `julia --project=$(@__DIR__) -e $(jl_expr)`
-        run(cm)
-        # task = Threads.@spawn run(cm)
-        # push!(tasks, task)
+        return Literate.markdown(
+            p_, joinpath(OUTPUT, d); execute=true, name, documenter=true,
+            preprocess=Base.Fix1(preprocess, p_))
     end
-    # return wait.(tasks)
 end
