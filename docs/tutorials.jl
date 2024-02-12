5e731fbbda30775f1926ae2209e6c697fb49e9ec
Avik Pal <avikpal@mit.edu>
2024-02-12 12:44:20 -0500

    Add Downgrade CI

--- docs/tutorials.jl
+++ docs/tutorials.jl
@@ -12,8 +12,6 @@ end
 
 @everywhere get_example_path(p) = joinpath(@__DIR__, "..", "examples", p)
 
-OUTPUT = joinpath(@__DIR__, "src", "tutorials")
-
 BEGINNER_TUTORIALS = ["Basics/main.jl", "PolynomialFitting/main.jl", "SimpleRNN/main.jl"]
 INTERMEDIATE_TUTORIALS = ["NeuralODE/main.jl", "BayesianNN/main.jl", "HyperNet/main.jl"]
 ADVANCED_TUTORIALS = ["GravitationalWaveForm/main.jl"]
@@ -27,6 +25,7 @@ pmap(enumerate(TUTORIALS)) do (i, (d, p))
     withenv("JULIA_DEBUG" => "Literate") do
         name = "$(i)_$(first(rsplit(p, "/")))"
         p_ = get_example_path(p)
+        OUTPUT = joinpath(@__DIR__, "src", "tutorials")
         return Literate.markdown(
             p_, joinpath(OUTPUT, d); execute=true, name, documenter=true,
             preprocess=Base.Fix1(preprocess, p_))
