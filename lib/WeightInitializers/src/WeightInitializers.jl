ca3317642b8cec50eed1758d7a2ee4e4419fbf2d
Avik Pal <avikpal@mit.edu>
2024-02-13 10:38:15 -0500

    Mark the initialization functions as non-differentiable

--- lib/WeightInitializers/src/WeightInitializers.jl
+++ lib/WeightInitializers/src/WeightInitializers.jl
@@ -3,11 +3,20 @@ module WeightInitializers
 import PrecompileTools: @recompile_invalidations
 
 @recompile_invalidations begin
-    using PartialFunctions, Random, SpecialFunctions, Statistics
+    using ChainRulesCore, PartialFunctions, Random, SpecialFunctions, Statistics
 end
 
 include("utils.jl")
 include("initializers.jl")
+
+# Mark the functions as non-differentiable
+for f in [
+    :zeros64, :ones64, :rand64, :randn64, :zeros32, :ones32, :rand32, :randn32, :zeros16,
+    :ones16, :rand16, :randn16, :zerosC64, :onesC64, :randC64, :randnC64, :zerosC32,
+    :onesC32, :randC32, :randnC32, :zerosC16, :onesC16, :randC16, :randnC16, :glorot_normal,
+    :glorot_uniform, :kaiming_normal, :kaiming_uniform, :truncated_normal]
+    @eval @non_differentiable $(f)(::Any...)
+end
 
 export zeros64, ones64, rand64, randn64, zeros32, ones32, rand32, randn32, zeros16, ones16,
        rand16, randn16
