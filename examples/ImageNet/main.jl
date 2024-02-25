46a447b791530a2e585ff6776f2be5842b75a3d0
Avik Pal <avik.pal.2017@gmail.com>
2024-02-24 19:45:20 -0500

    Update examples/ImageNet/main.jl

--- examples/ImageNet/main.jl
+++ examples/ImageNet/main.jl
@@ -2,8 +2,8 @@
 
 using Augmentor                                         # Image Augmentation
 using Boltz                                             # Computer Vision Models
-import Flux                                              # Some Blotz Models need Flux
-import Metalhead                                         # Some Blotz Models need MetalHead
+import Flux                                             # Some Blotz Models need Flux
+import Metalhead                                        # Some Blotz Models need MetalHead
 using Configurations                                    # Experiment Configurations
 using LuxAMDGPU                                         # AMDGPUs <3
 using LuxCUDA                                           # NVIDIA GPUs <3
