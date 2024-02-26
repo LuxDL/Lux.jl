1303c8e4be37f9f701167ccde6c2349d0154f670
MartinuzziFrancesco <martinuzzi.francesco@gmail.com>
2024-02-26 11:27:11 +0100

    import fixes, adding inits to non-diffs list

--- lib/WeightInitializers/src/WeightInitializers.jl
+++ lib/WeightInitializers/src/WeightInitializers.jl
@@ -1,10 +1,9 @@
 module WeightInitializers
 
 import PrecompileTools: @recompile_invalidations
-using PartialFunctions, Random, SpecialFunctions, Statistics, LinearAlgebra
 
 @recompile_invalidations begin
-    using ChainRulesCore, PartialFunctions, Random, SpecialFunctions, Statistics
+    using ChainRulesCore, PartialFunctions, Random, SpecialFunctions, Statistics, LinearAlgebra
 end
 
 include("utils.jl")
@@ -15,7 +14,8 @@ for f in [
     :zeros64, :ones64, :rand64, :randn64, :zeros32, :ones32, :rand32, :randn32, :zeros16,
     :ones16, :rand16, :randn16, :zerosC64, :onesC64, :randC64, :randnC64, :zerosC32,
     :onesC32, :randC32, :randnC32, :zerosC16, :onesC16, :randC16, :randnC16, :glorot_normal,
-    :glorot_uniform, :kaiming_normal, :kaiming_uniform, :truncated_normal]
+    :glorot_uniform, :kaiming_normal, :kaiming_uniform, :truncated_normal, :orthogonal,
+    :sparse_init, :identity_init]
     @eval @non_differentiable $(f)(::Any...)
 end
 
