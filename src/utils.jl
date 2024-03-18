850610303d83a8c72cdaa119fe02a201030cb93d
Avik Pal <avikpal@mit.edu>
2024-03-18 13:54:45 -0400

    Add warning for incorrect uses of f<__> functions

--- src/utils.jl
+++ src/utils.jl
@@ -212,6 +212,26 @@ for (fname, ftype) in zip((:f16, :f32, :f64), (Float16, Float32, Float64))
     end
 end
 
+# Common incorrect usage
+for f in (f16, f32, f64)
+    warn_msg = "$(f) is not meant to be broadcasted like `$(f).(x)` or `x .|> $(f)`, and \
+                this might give unexpected results and could lead to crashes. Directly use \
+                `$(f)` as `$(f)(x)` or `x |> $(f)` instead."
+    @eval begin
+        function Base.Broadcast.broadcasted(::typeof($(f)), arg1)
+            @warn $(warn_msg)
+            arg1′ = Broadcast.broadcastable(arg1)
+            return Broadcast.broadcasted(Broadcast.combine_styles(arg1′), $(f), arg1′)
+        end
+
+        function Base.Broadcast.broadcasted(::typeof(|>), arg1, ::typeof($(f)))
+            @warn $(warn_msg)
+            arg1′ = Broadcast.broadcastable(arg1)
+            return Broadcast.broadcasted(Broadcast.combine_styles(arg1′), $(f), arg1′)
+        end
+    end
+end
+
 # Used in freezing
 ## Extend for custom types
 @inline _pairs(x) = pairs(x)
