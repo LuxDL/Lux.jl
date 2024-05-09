81b2954e09d540c15d4f5fdc33ac13f4edddf7e5
Avik Pal <avikpal@mit.edu>
2024-05-08 20:03:24 -0400

    Remove custom rrule

--- ext/LuxDynamicExpressionsExt.jl
+++ ext/LuxDynamicExpressionsExt.jl
@@ -1,9 +1,12 @@
 module LuxDynamicExpressionsExt
 
-using ChainRulesCore: NoTangent
-using DynamicExpressions: Node, OperatorEnum, eval_grad_tree_array
+using ChainRulesCore: ChainRulesCore, NoTangent
+using DynamicExpressions: DynamicExpressions, Node, OperatorEnum, eval_grad_tree_array,
+                          eval_tree_array
 using FastClosures: @closure
 using Lux: Lux, NAME_TYPE, Chain, Parallel, WrappedFunction
+
+const CRC = ChainRulesCore
 
 @inline Lux._is_extension_loaded(::Val{:DynamicExpressions}) = true
 
@@ -30,12 +33,14 @@ end
 function Lux.__apply_dynamic_expression_rrule(
         de::Lux.DynamicExpressionsLayer, expr, operator_enum, x, ps)
     Lux.__update_expression_constants!(expr, ps)
-    _, Jₓ, _ = eval_grad_tree_array(expr, x, operator_enum; variable=Val(true), de.turbo)
-    y, Jₚ, _ = eval_grad_tree_array(expr, x, operator_enum; variable=Val(false), de.turbo)
+    @static if pkgversion(DynamicExpressions) < v"0.17"
+        error("`DynamicExpressions` v0.17 or later is required for reverse mode to work.")
+    end
+    (y, _), pb_f = CRC.rrule(eval_tree_array, expr, x, operator_enum; de.turbo, de.bumper)
     __∇apply_dynamic_expression = @closure Δ -> begin
-        ∂x = Jₓ .* reshape(Δ, 1, :)
-        ∂ps = Jₚ * Δ
-        return NoTangent(), NoTangent(), NoTangent(), NoTangent(), ∂x, ∂ps, NoTangent()
+        _, ∂expr, ∂x, ∂operator_enum = pb_f((Δ, nothing))
+        ∂ps = CRC.unthunk(∂expr).gradient
+        return NoTangent(), NoTangent(), NoTangent(), ∂operator_enum, ∂x, ∂ps, NoTangent()
     end
     return y, __∇apply_dynamic_expression
 end
