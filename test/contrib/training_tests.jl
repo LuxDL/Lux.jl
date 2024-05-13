aa089be8c74a665971e5d814575b128b52955882
Avik Pal <avikpal@mit.edu>
2024-05-12 22:31:44 -0400

    Add tests

--- test/contrib/training_tests.jl
+++ test/contrib/training_tests.jl
@@ -3,7 +3,7 @@
 
     rng = get_stable_rng(12345)
 
-    @testset "$mode" for (mode, aType, device, ongpu) in MODES
+    @testset "$mode" for (mode, aType, dev, ongpu) in MODES
         model = Dense(3, 2)
         opt = Adam(0.01f0)
 
@@ -32,12 +32,12 @@ end
 
     rng = get_stable_rng(12345)
 
-    @testset "$mode" for (mode, aType, device, ongpu) in MODES
+    @testset "$mode" for (mode, aType, dev, ongpu) in MODES
         model = Dense(3, 2)
         opt = Adam(0.01f0)
 
         tstate = Lux.Experimental.TrainState(
-            Lux.replicate(rng), model, opt; transform_variables=device)
+            Lux.replicate(rng), model, opt; transform_variables=dev)
 
         x = randn(Lux.replicate(rng), Float32, (3, 1)) |> aType
 
@@ -49,6 +49,51 @@ end
             tstate_ = Lux.Experimental.apply_gradients(tstate, grads)
             @test tstate_.step == 1
             @test tstate != tstate_
+        end
+    end
+end
+
+@testitem "Training API" setup=[SharedTestSetup] tags=[:contrib] begin
+    using ADTypes, Optimisers
+    import Enzyme, Tracker, ReverseDiff, Zygote
+
+    function mse(model, ps, st, data)
+        x_data, y_data = data
+        y, st_ = model(x_data, ps, st)
+        return sum(abs2, y .- y_data), st_, ()
+    end
+
+    rng = get_stable_rng(12345)
+
+    x_data = randn(rng, Float32, 4, 32)
+    y_data = evalpoly.(x_data, ((1, 2, 3),)) .- evalpoly.(x_data, ((5, 2),))
+    y_data = (y_data .- minimum(y_data)) ./ (maximum(y_data) - minimum(y_data))
+    dataset = [(x_data[:, i], y_data[:, i]) for i in Iterators.partition(1:32, 8)]
+
+    @testset "$mode" for (mode, aType, dev, ongpu) in MODES
+        model = Chain(Dense(4, 32, tanh), BatchNorm(32),
+            Dense(32, 32, tanh), BatchNorm(32), Dense(32, 1))
+        dataset_ = [dev((x, y)) for (x, y) in dataset]
+        opt = Adam(0.001f0)
+
+        @testset "$(ad)" for ad in (
+            AutoZygote(), AutoTracker(), AutoReverseDiff(), AutoEnzyme())
+            ongpu && (ad isa AutoReverseDiff || ad isa AutoEnzyme) && continue
+
+            tstate = Lux.Experimental.TrainState(
+                Lux.replicate(rng), model, opt; transform_variables=dev)
+
+            initial_loss = first(mse(model, tstate.parameters, tstate.states, dataset_[1]))
+
+            for epoch in 1:100, (x, y) in dataset_
+                grads, loss, _, tstate = Lux.Experimental.compute_gradients(
+                    ad, mse, (x, y), tstate)
+                tstate = Lux.Experimental.apply_gradients(tstate, grads, true)
+            end
+
+            final_loss = first(mse(model, tstate.parameters, tstate.states, dataset_[1]))
+
+            @test final_loss * 100 < initial_loss
         end
     end
 end
