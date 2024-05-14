b33aefe0cf8bd91ebd78be99446bf046df6de55c
Avik Pal <avikpal@mit.edu>
2024-05-14 16:17:02 -0400

    Update ext/LuxZygoteExt/nested_ad.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ext/LuxZygoteExt/nested_ad.jl
+++ ext/LuxZygoteExt/nested_ad.jl
@@ -24,7 +24,7 @@ for fType in Lux.AD_CONVERTIBLE_FUNCTIONS
 
         @eval @inline function Lux.__batched_jacobian(f::$(fType), backend::AutoZygote, x)
             f_internal, y = Lux.__rewrite_ad_call(f)
-            jac_fn = let backend=backend
+            jac_fn = let backend = backend
                 (f, x_in) -> Lux.__batched_jacobian_impl(f, backend, x_in)
             end
             return Lux.__internal_ad_jacobian_call(
