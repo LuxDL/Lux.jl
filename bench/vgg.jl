c3fc68e691b5c9405de3fbd6556b5d39dbbba969
Avik Pal <avikpal@mit.edu>
2024-06-04 21:44:50 -0700

    Minor optimizations

--- bench/vgg.jl
+++ bench/vgg.jl
@@ -1,21 +1,33 @@
 function add_vgg_benchmarks!()
-    vgg16 = Chain(Conv((3, 3), 3 => 64, relu; pad=(1, 1), stride=(1, 1)), BatchNorm(64),
-        Conv((3, 3), 64 => 64, relu; pad=(1, 1), stride=(1, 1)), BatchNorm(64),
-        MaxPool((2, 2)), Conv((3, 3), 64 => 128, relu; pad=(1, 1), stride=(1, 1)),
-        BatchNorm(128), Conv((3, 3), 128 => 128, relu; pad=(1, 1), stride=(1, 1)),
-        BatchNorm(128), MaxPool((2, 2)),
-        Conv((3, 3), 128 => 256, relu; pad=(1, 1), stride=(1, 1)),
-        BatchNorm(256), Conv((3, 3), 256 => 256, relu; pad=(1, 1), stride=(1, 1)),
-        BatchNorm(256), Conv((3, 3), 256 => 256, relu; pad=(1, 1), stride=(1, 1)),
-        BatchNorm(256), MaxPool((2, 2)),
-        Conv((3, 3), 256 => 512, relu; pad=(1, 1), stride=(1, 1)), BatchNorm(512),
-        Conv((3, 3), 512 => 512, relu; pad=(1, 1), stride=(1, 1)), BatchNorm(512),
-        Conv((3, 3), 512 => 512, relu; pad=(1, 1), stride=(1, 1)), BatchNorm(512),
-        MaxPool((2, 2)), Conv((3, 3), 512 => 512, relu; pad=(1, 1), stride=(1, 1)),
-        BatchNorm(512), Conv((3, 3), 512 => 512, relu; pad=(1, 1), stride=(1, 1)),
-        BatchNorm(512), Conv((3, 3), 512 => 512, relu; pad=(1, 1), stride=(1, 1)),
-        BatchNorm(512), MaxPool((2, 2)), FlattenLayer(), Dense(512, 4096, relu),
-        Dropout(0.5), Dense(4096, 4096, relu), Dropout(0.5), Dense(4096, 10))
+    # Julia Fails to Specialize on Large NTs, so split it up
+    function conv_bn(ksize, (in_chs, out_chs)::Pair, args...; kwargs...)
+        return Chain(
+            Conv(ksize, in_chs => out_chs, args...; kwargs...), BatchNorm(out_chs);
+            name="ConvBN")
+    end
+
+    vgg16 = Chain(
+        Chain(conv_bn((3, 3), 3 => 64, relu; pad=(1, 1), stride=(1, 1)),
+            conv_bn((3, 3), 64 => 64, relu; pad=(1, 1), stride=(1, 1)),
+            MaxPool((2, 2)),
+            conv_bn((3, 3), 64 => 128, relu; pad=(1, 1), stride=(1, 1)),
+            conv_bn((3, 3), 128 => 128, relu; pad=(1, 1), stride=(1, 1)),
+            MaxPool((2, 2)),
+            conv_bn((3, 3), 128 => 256, relu; pad=(1, 1), stride=(1, 1)),
+            conv_bn((3, 3), 256 => 256, relu; pad=(1, 1), stride=(1, 1)),
+            conv_bn((3, 3), 256 => 256, relu; pad=(1, 1), stride=(1, 1)),
+            MaxPool((2, 2)),
+            conv_bn((3, 3), 256 => 512, relu; pad=(1, 1), stride=(1, 1)),
+            conv_bn((3, 3), 512 => 512, relu; pad=(1, 1), stride=(1, 1)),
+            conv_bn((3, 3), 512 => 512, relu; pad=(1, 1), stride=(1, 1)),
+            MaxPool((2, 2)),
+            conv_bn((3, 3), 512 => 512, relu; pad=(1, 1), stride=(1, 1)),
+            conv_bn((3, 3), 512 => 512, relu; pad=(1, 1), stride=(1, 1)),
+            conv_bn((3, 3), 512 => 512, relu; pad=(1, 1), stride=(1, 1)),
+            MaxPool((2, 2)); disable_optimizations=true),
+        FlattenLayer(),
+        Chain(Dense(512, 4096, relu), Dropout(0.5f0), Dense(4096, 4096, relu),
+            Dropout(0.5f0), Dense(4096, 10); name="Classifier"); disable_optimizations=true)
 
     flux_model = () -> Flux.Chain(
         Flux.Conv((3, 3), 3 => 64, relu; pad=(1, 1), stride=(1, 1)),
