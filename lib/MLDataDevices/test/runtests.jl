ad659c83921010ecc5fdd35d1d5da54f1daad569
Avik Pal <avikpal@mit.edu>
2024-06-05 20:21:12 -0700

    Try installing packages only if needed

--- lib/MLDataDevices/test/runtests.jl
+++ lib/MLDataDevices/test/runtests.jl
@@ -1,21 +1,26 @@
+import Pkg
 using Aqua, SafeTestsets, Test, LuxDeviceUtils, TestSetExtensions
 
 const GROUP = get(ENV, "GROUP", "NONE")
 
 @testset ExtendedTestSet "LuxDeviceUtils Tests" begin
     if GROUP == "CUDA" || GROUP == "ALL"
+        Pkg.add("LuxCUDA")
         @safetestset "CUDA" include("cuda.jl")
     end
 
     if GROUP == "AMDGPU" || GROUP == "ALL"
+        Pkg.add("LuxAMDGPU")
         @safetestset "AMDGPU" include("amdgpu.jl")
     end
 
     if GROUP == "Metal" || GROUP == "ALL"
+        Pkg.add("Metal")
         @safetestset "Metal" include("metal.jl")
     end
 
     if GROUP == "oneAPI" || GROUP == "ALL"
+        Pkg.add("oneAPI")
         @safetestset "oneAPI" include("oneapi.jl")
     end
 
@@ -24,6 +29,7 @@ const GROUP = get(ENV, "GROUP", "NONE")
 
         @safetestset "Component Arrays" include("component_arrays.jl")
 
+        Pkg.add(["LuxCUDA", "LuxAMDGPU", "Metal", "oneAPI"])
         @safetestset "Explicit Imports" include("explicit_imports.jl")
     end
 end
