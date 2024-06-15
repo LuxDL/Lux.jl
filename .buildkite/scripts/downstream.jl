d4b044bbe859e2bb9b6c2116e24769bf47aa6f5d
Avik Pal <avikpal@mit.edu>
2024-06-15 16:31:06 -0700

    Move downstream testing code to a script

---
+++ .buildkite/scripts/downstream.jl
@@ -0,0 +1,25 @@
+using Pkg
+
+repo = ARGS[1]
+if contains(repo, "#")
+    repo, group = split(repo, "#")
+else
+    group = ARGS[2]
+end
+
+println("--- :julia: Instantiating project")
+withenv("JULIA_PKG_PRECOMPILE_AUTO" => 0, "GROUP" => group, "BACKEND_GROUP" => group) do
+    Pkg.instantiate()
+
+    try
+        Pkg.develop(repo)
+        println("+++ :julia: Running tests")
+        Pkg.test("$(repo)"; coverage=true)
+    catch err
+        err isa Pkg.Resolve.ResolverError || rethrow()
+        @info "Not compatible with this release. No problem." exception=err
+        exit(0)
+    end
+end
+
+println("+++ :julia: Finished Downstream Test")
