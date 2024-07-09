3c0d3b639b1f1e5f43df4155de59dde28d16c1ad
Avik Pal <avikpal@mit.edu>
2024-07-08 23:41:18 -0700

    ci: more robust testing and ci (#28)
    * test: more explicit imports testing
    
    * ci: run only necessary tests

--- lib/WeightInitializers/test/qa_tests.jl
+++ lib/WeightInitializers/test/qa_tests.jl
@@ -11,6 +11,16 @@ end
     @test check_no_implicit_imports(WeightInitializers) === nothing
     @test check_no_stale_explicit_imports(WeightInitializers) === nothing
     @test check_no_self_qualified_accesses(WeightInitializers) === nothing
+    @test check_all_explicit_imports_via_owners(WeightInitializers) === nothing
+    @test check_all_qualified_accesses_via_owners(WeightInitializers) === nothing
+    @test_broken check_all_explicit_imports_are_public(WeightInitializers) === nothing  # mostly upstream problems
+
+    try  # FIXME: Soft fail for now
+        acc = check_all_qualified_accesses_are_public(WeightInitializers)
+        @test acc === nothing
+    catch
+        @test_broken check_all_qualified_accesses_are_public(WeightInitializers) === nothing
+    end
 end
 
 @testitem "doctests: Quality Assurance" begin
