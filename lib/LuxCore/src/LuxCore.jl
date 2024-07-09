2965af4d47664a3ffb00d33066d12c243a4bc06b
Avik Pal <avikpal@mit.edu>
2024-07-08 19:00:36 -0700

    feat: use dispatch doctor on `apply`

--- lib/LuxCore/src/LuxCore.jl
+++ lib/LuxCore/src/LuxCore.jl
@@ -1,8 +1,12 @@
 module LuxCore
 
+using ChainRulesCore: ChainRulesCore, HasReverseMode, RuleConfig
+using DispatchDoctor: @stable
 using Functors: Functors, fmap
 using Random: Random, AbstractRNG, Xoshiro
 using Setfield: Setfield
+
+const CRC = ChainRulesCore
 
 # PRNG Handling
 """
@@ -171,8 +175,24 @@ this include:
     we can unpack the input in `apply` and pass it to the appropriate layer and then
     repack it before returning. See the Lux manual on Custom Input Types for a motivating
     example.
+
+!!! tip
+
+    `apply` is integrated with `DispatchDoctor.jl` that allows automatic verification of
+    type stability. By default this is "disable"d. For more information, see the
+    [documentation](https://github.com/MilesCranmer/DispatchDoctor.jl).
 """
-apply(model::AbstractExplicitLayer, x, ps, st) = model(x, ps, st)
+@stable default_mode="disable" function apply(model::AbstractExplicitLayer, x, ps, st)
+    return _apply(model, x, ps, st)
+end
+
+# FIXME: See https://github.com/MilesCranmer/DispatchDoctor.jl/issues/46
+function CRC.rrule(cfg::RuleConfig{>:HasReverseMode}, ::typeof(apply),
+        model::AbstractExplicitLayer, x, ps, st)
+    return CRC.rrule_via_ad(cfg, _apply, model, x, ps, st)
+end
+
+_apply(model::AbstractExplicitLayer, x, ps, st) = model(x, ps, st)
 
 """
     stateless_apply(model, x, ps)
