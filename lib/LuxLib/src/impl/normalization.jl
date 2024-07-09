3519fb8284b3eb53c4522f35e0b67eadf2a64f9b
Avik Pal <avikpal@mit.edu>
2024-07-09 07:58:11 -0700

    fix: upstream fix of zygote type stability (#81)

--- lib/LuxLib/src/impl/normalization.jl
+++ lib/LuxLib/src/impl/normalization.jl
@@ -52,13 +52,7 @@ function _normalization_impl(x::AbstractArray, running_mean::Optional{<:Abstract
     return _affine_normalize(act, x, μ, σ², scale, bias, epsilon), rμ, rσ²
 end
 
-# FIXME: See https://github.com/MilesCranmer/DispatchDoctor.jl/issues/46
 @stable default_mode="warn" _normalization(args...)=__normalization(args...)
-
-function CRC.rrule(
-        cfg::CRC.RuleConfig{>:CRC.HasReverseMode}, ::typeof(_normalization), args...)
-    return CRC.rrule_via_ad(cfg, __normalization, args...)
-end
 
 function __normalization(x::AbstractArray, running_mean::Optional{<:AbstractVector},
         running_var::Optional{<:AbstractVector}, scale::Optional{<:AbstractVector},
