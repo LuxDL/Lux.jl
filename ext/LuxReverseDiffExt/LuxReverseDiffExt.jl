5e83ae4dcd15ae3465d20a3b71c613a86b9f6622
Avik Pal <avikpal@mit.edu>
2024-07-09 19:04:27 -0700

    fix: handle reverse for operator overloading AD

--- ext/LuxReverseDiffExt/LuxReverseDiffExt.jl
+++ ext/LuxReverseDiffExt/LuxReverseDiffExt.jl
@@ -26,6 +26,11 @@ Lux.apply(m::Lux.AbstractExplicitLayer, x::TrackedArray, ps, st) = m(x, ps, st)
 @inline Lux.__eltype(::TrackedReal{T}) where {T} = T
 @inline Lux.__eltype(::AbstractArray{<:TrackedReal{T}}) where {T} = T
 
+@inline Lux.__reverse(x::TrackedArray; dims=:) = ArrayInterface.aos_to_soa(reverse(x; dims))
+@inline function Lux.__reverse(x::AbstractArray{<:TrackedReal}; dims=:)
+    return ArrayInterface.aos_to_soa(reverse(x; dims))
+end
+
 @inline function Lux.__convert_eltype(::Type{T}, x::AbstractArray{<:TrackedReal}) where {T}
     @warn "`Lux.__convert_eltype` doesn't support converting element types of ReverseDiff \
            `TrackedReal` arrays. Currently this is a no-op." maxlog=1
