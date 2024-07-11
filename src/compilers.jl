60d2181011f4aa0f814e71309d4c1579add5360b
Avik Pal <avikpal@mit.edu>
2024-07-10 21:39:30 -0700

    feat(reactant): auto compile inference mode if possible
    [skip ci]

--- src/compilers.jl
+++ src/compilers.jl
@@ -1,15 +1,29 @@
 abstract type AbstractCompilerBackend end
 
 """
-    ReactantBackend()
+    ReactantBackend(; input_prototype = nothing)
 
 Compile Lux model and gradient computation to MLIR/XLA via `Reactant.jl`.
 
-This has been added to Lux very recently and is under-going rapid development. Currently,
-only a limited subset of Lux models can be compiled via `Reactant.jl`. If you encounter any
-issues, please report them on the `Lux.jl` or `Reactant.jl` GitHub repository.
+!!! tip "Newly Added Feature!"
+
+    This has been added to Lux very recently and is under-going rapid development.
+    Currently, only a limited subset of Lux models can be compiled via `Reactant.jl`. If you
+    encounter any issues, please report them on the `Lux.jl` or `Reactant.jl` GitHub
+    repository.
+
+## Keyword Arguments
+
+  - `input_prototype`: Input data representative of the data that will be used for
+    inference. If this is provided, we will compile the inference function with
+    `Reactant.jl` on the first call to [`Lux.Experimental.single_train_step!`](@ref) or
+    [`Lux.Experimental.single_train_step`](@ref). If this is not provided, we will have to
+    recompile the inference function on every call to `(::TrainState)(data)` and this will
+    be prohibitively expensive.
 
 See [`Lux.Experimental.single_train_step!`](@ref) or
 [`Lux.Experimental.single_train_step`](@ref) for information on how to use this backend.
 """
-struct ReactantBackend <: AbstractCompilerBackend end
+@kwdef @concrete struct ReactantBackend <: AbstractCompilerBackend
+    input_prototype = nothing
+end
