f665de65a51843eaffb87b6c2c809d93e35de790
Avik Pal <avikpal@mit.edu>
2024-07-10 21:08:36 -0700

    feat(training): add inference mode for `TrainState`
    [skip ci]

--- src/helpers/training.jl
+++ src/helpers/training.jl
@@ -13,7 +13,7 @@ using Random: AbstractRNG
 """
     TrainState
 
-Training State containing:
+## Training State containing:
 
   - `model`: `Lux` model.
   - `parameters`: Trainable Variables of the `model`.
@@ -22,7 +22,7 @@ Training State containing:
   - `optimizer_state`: Optimizer State.
   - `step`: Number of updates of the parameters made.
 
-Internal fields:
+## Internal fields:
 
   - `cache`: Cached values. Implementations are free to use this for whatever they want.
   - `objective_function`: Objective function might be cached.
@@ -31,6 +31,12 @@ Internal fields:
 
     Constructing this object directly shouldn't be considered a stable API. Use the
     version with the Optimisers API.
+
+## Special Features
+
+To run inference using the current parameters and states simply call the TrainState with
+the input data as `tstate(data)`. This will automatically set `Lux.testmode`. However, note
+that `tstate.states` will not be updated with the new state.
 """
 @concrete struct TrainState
     cache
@@ -84,6 +90,10 @@ function TrainState(
         model::AbstractExplicitLayer, ps, st, optimizer::Optimisers.AbstractRule)
     st_opt = Optimisers.setup(optimizer, ps)
     return TrainState(nothing, nothing, model, ps, st, optimizer, st_opt, 0)
+end
+
+function (tstate::TrainState)(data)
+    return first(tstate.model(data, tstate.parameters, Lux.testmode(tstate.states)))
 end
 
 @concrete struct TrainingBackendCache{backend, first_try}
