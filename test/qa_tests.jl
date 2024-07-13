edb7776b03b11580ac99418eb83fd5cea01c230a
Avik Pal <avikpal@mit.edu>
2024-07-13 10:54:10 -0700

    fix: qa testing

--- test/qa_tests.jl
+++ test/qa_tests.jl
@@ -1,8 +1,10 @@
 @testitem "Aqua: Quality Assurance" tags=[:others] begin
-    using Aqua, ChainRulesCore
+    using Aqua, ChainRulesCore, ForwardDiff
 
     Aqua.test_all(Lux; piracies=false, ambiguities=false)
-    Aqua.test_ambiguities(Lux; recursive=false)
+    Aqua.test_ambiguities(Lux;
+        exclude=[ForwardDiff.jacobian, ForwardDiff.gradient,
+            Lux.__batched_jacobian, Lux.__jacobian_vector_product_impl])
     Aqua.test_piracies(
         Lux; treat_as_own=[ChainRulesCore.frule, ChainRulesCore.rrule, Core.kwcall])
 end
