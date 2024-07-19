9648bf9b02c7634705f75f88479c8231f1c08f51
Avik Pal <avikpal@mit.edu>
2024-07-18 17:49:20 -0700

    docs: add bias_activation docs

--- lib/LuxLib/src/api/bias_activation.jl
+++ lib/LuxLib/src/api/bias_activation.jl
@@ -1,8 +1,28 @@
+"""
+    bias_activation(σ, x, bias)
+
+Applies the activation function `σ` elementwise to the result of broadcasted addition of `x`
+and `bias` along the penultimate dimension. A vector `x` is treated as a matrix with a
+single last dimension.
+
+## Arguments
+
+  - `σ`: Activation function
+  - `x`: Input to be transformed
+  - `bias`: Bias to be added. Can be `nothing`.
+"""
 function bias_activation(σ::F, x::AbstractArray, bias::Optional{<:AbstractVector}) where {F}
     _bias_act_check(x, bias)
     return __bias_activation_impl(σ, x, bias)
 end
 
+"""
+    bias_activation!!(σ, x, bias)
+
+Same as [`bias_activation`](@ref) but might update `x` in-place if possible. Users should
+not rely on `x` being mutated, it is recommended to use it like
+`y = bias_activation!!(σ, x, bias)`. If `x` is updated in-place, `y` aliases `x`.
+"""
 function bias_activation!!(
         σ::F, x::AbstractArray, bias::Optional{<:AbstractVector}) where {F}
     _bias_act_check(x, bias)
