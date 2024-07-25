551eba238d672a6d9bd1b896e4e83b8df91800ad
Avik Pal <avikpal@mit.edu>
2024-07-24 20:36:10 -0700

    feat: error on common mistakes

--- lib/LuxCore/ext/LuxCoreEnzymeCoreExt.jl
+++ lib/LuxCore/ext/LuxCoreEnzymeCoreExt.jl
@@ -1,9 +1,37 @@
 module LuxCoreEnzymeCoreExt
 
-using EnzymeCore: EnzymeRules
+using EnzymeCore: EnzymeCore, EnzymeRules
 using LuxCore: LuxCore
 using Random: AbstractRNG
 
 EnzymeRules.inactive(::typeof(LuxCore.replicate), ::AbstractRNG) = nothing
+
+# Handle common mistakes users might make
+const LAYER_DERIVATIVE_ERROR_MSG = """
+Lux Layers only support `EnzymeCore.Const` annotation.
+
+Lux Layers are immutable constants and gradients w.r.t. them are `nothing`. To
+compute the gradients w.r.t. the layer's parameters, use the first argument returned
+by `LuxCore.setup(rng, layer)` instead.
+"""
+
+function EnzymeCore.Active(::LuxCore.AbstractExplicitLayer)
+    throw(ArgumentError(LAYER_DERIVATIVE_ERROR_MSG))
+end
+
+for annotation in (:Duplicated, :DuplicatedNoNeed)
+    @eval function EnzymeCore.$(annotation)(
+            ::LuxCore.AbstractExplicitLayer, ::LuxCore.AbstractExplicitLayer)
+        throw(ArgumentError(LAYER_DERIVATIVE_ERROR_MSG))
+    end
+end
+
+for annotation in (:BatchDuplicated, :BatchDuplicatedNoNeed)
+    @eval function EnzymeCore.$(annotation)(
+            ::LuxCore.AbstractExplicitLayer, ::NTuple{N, <:LuxCore.AbstractExplicitLayer},
+            check::Bool=true) where {N}
+        throw(ArgumentError(LAYER_DERIVATIVE_ERROR_MSG))
+    end
+end
 
 end
