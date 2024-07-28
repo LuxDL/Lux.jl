38e4abb3bfa9ae1add144bff11ab6b0d38046435
Avik Pal <avikpal@mit.edu>
2024-07-28 11:36:56 -0700

    fix: enable parallel testing

--- lib/LuxTestUtils/test/runtests.jl
+++ lib/LuxTestUtils/test/runtests.jl
@@ -1,3 +1,8 @@
-using ReTestItems
+using InteractiveUtils, Hwloc, ReTestItems
 
-ReTestItems.runtests(@__DIR__)
+@info sprint(io -> versioninfo(io; verbose=true))
+
+const RETESTITEMS_NWORKERS = parse(
+    Int, get(ENV, "RETESTITEMS_NWORKERS", string(min(Hwloc.num_physical_cores(), 16))))
+
+ReTestItems.runtests(@__DIR__; nworkers=RETESTITEMS_NWORKERS)
