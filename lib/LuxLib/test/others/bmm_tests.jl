acc141df7578edbfa84c5730bb33bd90b670c62f
Avik Pal <avikpal@mit.edu>
2024-08-04 07:59:07 -0700

    test: fix dimensions

--- lib/LuxLib/test/others/bmm_tests.jl
+++ lib/LuxLib/test/others/bmm_tests.jl
@@ -150,21 +150,22 @@ end
     SharedTestSetup, BatchedMMSetup] begin
     rng = StableRNG(1234)
 
-    sizes = []
-    for sA in [(1, 1), (1, 3), (3, 1), (3, 3)], sB in [(1, 1), (1, 3), (3, 1), (3, 3)]
-        sA[2] == sB[1] && push!(sizes, (sA, sB))
-    end
-
     @testset "$mode" for (mode, aType, ongpu) in MODES
         @testset "Float64 × $(TB)" for TB in [Float64, ComplexF64]
             @testset "trivial dimensions & unit strides" begin
                 @testset "$tA(rand$((sA...,3))) ⊠ $tB(rand$((sB...,3)))" for tA in [
                         identity, batched_adjoint, batched_transpose, perm_12, perm_23],
+                    sA in [(1, 1), (1, 3), (3, 1), (3, 3)],
                     tB in [identity, batched_adjoint, batched_transpose, perm_12, perm_23],
-                    (sA, sB) in sizes
+                    sB in [(1, 1), (1, 3), (3, 1), (3, 3)]
 
                     A = tA(rand(rng, TB, sA..., 3)) |> aType
                     B = tB(rand(rng, TB, sB..., 3)) |> aType
+
+                    if size(A, 2) != size(B, 1) || size(A, 3) != 3 || size(B, 3) != 3 
+                        @test true # avoid a warning in ReTestItems.jl
+                        continue
+                    end
 
                     C = cat(A[:, :, 1] * B[:, :, 1], A[:, :, 2] * B[:, :, 2],
                         A[:, :, 3] * B[:, :, 3]; dims=3)
