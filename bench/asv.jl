f0f3749348beb417a432d05868f581d904e7faac
Avik Pal <avikpal@mit.edu>
2024-08-04 17:15:46 -0700

    ci: add CI to benchmark load times
    [skip docs] [skip tests]

---
+++ bench/asv.jl
@@ -0,0 +1,17 @@
+# This is mostly a stub file. Allows up to use AirSpeedVelocity.jl to test package load
+# times
+
+using BenchmarkTools, Lux, Random
+
+const SUITE = BenchmarkGroup()
+
+SUITE["basics"] = BenchmarkGroup()
+
+SUITE["basics"]["overhead"] = @benchmarkable begin
+    dense(x, ps, st)
+end setup=begin
+    dense = Dense(2, 3)
+    x = ones(Float32, 2, 3)
+    ps, st = Lux.setup(Xoshiro(), dense)
+    dense(x, ps, st)
+end
