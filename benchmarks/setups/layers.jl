7f864174ac8d23e89773a3d8c4be99ef558753cc
Avik Pal <avikpal@mit.edu>
2024-08-04 20:06:15 -0700

    perf: move more benchmarks over
    [skip tests] [skip docs]

--- benchmarks/setups/layers.jl
+++ benchmarks/setups/layers.jl
@@ -1,15 +1,43 @@
 function add_dense_benchmarks!(suite::BenchmarkGroup, group::String)
-    for n in (16,) #  128, 512, 2048), act in (identity, relu, gelu)
-        act = identity
+    for n in (16, 128, 512, 2048), act in (identity, relu, gelu)
+        n > 512 && group == "AMDGPU" && continue
+
         layer = Dense(n => n, act)
         simple_chains = n ≤ 200 ? Lux.ToSimpleChainsAdaptor((static(n),)) : nothing
         flux_model = () -> Flux.Dense(n => n, act)
 
         benchmark_forward_pass!(suite, group, "Dense($n => $n, $act)($n x 128)",
             layer, (n, 128); simple_chains, flux_model)
+
         benchmark_reverse_pass!(
             suite, group, [AutoZygote(), AutoEnzyme()], "Dense($n => $n, $act)($n x 128)",
             layer, (n, 128); simple_chains, flux_model)
+    end
+    return
+end
+
+function add_conv_benchmarks!(suite::BenchmarkGroup, group::String)
+    for ch in (2, 4, 32, 128), act in (identity, relu, gelu)
+        ch > 32 && group == "AMDGPU" && continue
+
+        if group == "AMDGPU"
+            layer = CrossCor((3, 3), ch => ch, act)
+            flux_model = () -> Flux.CrossCor((3, 3), ch => ch, act)
+        else
+            layer = Conv((3, 3), ch => ch, act)
+            flux_model = () -> Flux.Conv((3, 3), ch => ch, act)
+        end
+        simple_chains = ch ≤ 16 ?
+                        Lux.ToSimpleChainsAdaptor((static(64), static(64), static(ch))) :
+                        nothing
+
+        benchmark_forward_pass!(
+            suite, group, "Conv((3, 3), $ch => $ch, $act)(64 x 64 x $ch x 128)",
+            layer, (64, 64, ch, 128); simple_chains, flux_model)
+
+        benchmark_reverse_pass!(suite, group, [AutoZygote(), AutoEnzyme()],
+            "Conv((3, 3), $ch => $ch, $act)(64 x 64 x $ch x 128)",
+            layer, (64, 64, ch, 128); simple_chains, flux_model)
     end
     return
 end
