cdaeac244afde830c1b37aa244678c205721644a
Avik Pal <avikpal@mit.edu>
2024-08-05 17:23:59 -0700

    ci: attempt to cache the parameters
    [skip tests] [skip docs]

--- benchmarks/runbenchmarks.jl
+++ benchmarks/runbenchmarks.jl
@@ -1,7 +1,7 @@
 using BenchmarkTools: BenchmarkTools, BenchmarkGroup, @btime, @benchmarkable
 using InteractiveUtils: versioninfo
 using LinearAlgebra: BLAS
-using Statistics: minimum
+using Statistics: median
 using Suppressor: @suppress
 using ThreadPinning: pinthreads
 
@@ -14,9 +14,12 @@ BLAS.set_num_threads(Threads.nthreads() ÷ 2)
 
 const SUITE = BenchmarkGroup()
 
+BenchmarkTools.DEFAULT_PARAMETERS.seconds = 10
+
 # To run benchmarks on a specific GPU backend, add AMDGPU / CUDA / Metal / oneAPI
 # to benchmarks/Project.toml and change BENCHMARK_GROUP to the backend name
 const BENCHMARK_GROUP = get(ENV, "BENCHMARK_GROUP", "CPU")
+const FORCE_BENCHMARK_TUNING = parse(Bool, get(ENV, "FORCE_BENCHMARK_TUNING", "false"))
 const BENCHMARK_CPU_THREADS = Threads.nthreads()
 
 # Number of CPU threads to benchmarks on
@@ -45,15 +48,41 @@ include("setups/models.jl")
 include("setup.jl")
 setup_benchmarks(SUITE, BENCHMARK_GROUP, BENCHMARK_CPU_THREADS)
 
-BenchmarkTools.tune!(SUITE; verbose=true)
-results = BenchmarkTools.run(SUITE; verbose=true)
-filepath = joinpath(dirname(@__FILE__), "results")
+filepath = joinpath(@__DIR__, "results")
 mkpath(filepath)
 filename = BENCHMARK_GROUP == "CPU" ?
            string("CPUbenchmarks", BENCHMARK_CPU_THREADS, "threads.json") :
            string(BENCHMARK_GROUP, "benchmarks.json")
-BenchmarkTools.save(joinpath(filepath, filename), minimum(results))
+params_filename = BENCHMARK_GROUP == "CPU" ?
+                  string("CPUbenchmarks", BENCHMARK_CPU_THREADS, "threads_params.json") :
+                  string(BENCHMARK_GROUP, "benchmarks_params.json")
 
-display(minimum(results))
+try
+    params_file_url = "https://raw.githubusercontent.com/LuxDL/Lux.jl/gh-pages/benchmark-params/$(params_filename)"
+    @info "Downloaded params file from $params_file_url"
+    global download_path = download(params_file_url, params_filename)
+catch err
+    @error "Failed to download params file from \
+            https://raw.githubusercontent.com/LuxDL/Lux.jl/gh-pages/benchmark-params/$(params_filename)" err
+    global download_path = nothing
+end
 
+if download_path !== nothing && !FORCE_BENCHMARK_TUNING
+    @info "Loading params from $download_path"
+    _, has_all_groups = loadparams!(
+        SUITE, BenchmarkTools.load(download_path)[1], :evals)
+    if !has_all_groups
+        @warn "Some groups did not have all benchmarks. Tuning benchmarks."
+        BenchmarkTools.tune!(SUITE; verbose=true)
+    end
+else
+    @info "Tuning benchmarks"
+    BenchmarkTools.tune!(SUITE; verbose=true)
+end
+results = BenchmarkTools.run(SUITE; verbose=true)
+
+BenchmarkTools.save(joinpath(filepath, params_filename), BenchmarkTools.params(SUITE))
+@info "Saved params to $(joinpath(filepath, params_filename))"
+
+BenchmarkTools.save(joinpath(filepath, filename), median(results))
 @info "Saved results to $(joinpath(filepath, filename))"
