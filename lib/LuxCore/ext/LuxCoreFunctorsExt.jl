895c3c6dc1c85db8d7621721bff7e73f12896f08
Avik Pal <avikpal@mit.edu>
2024-08-17 21:17:36 -0700

    fix: update removed API

--- lib/LuxCore/ext/LuxCoreFunctorsExt.jl
+++ lib/LuxCore/ext/LuxCoreFunctorsExt.jl
@@ -5,26 +5,26 @@ using Functors: Functors
 
 LuxCore.Internal.is_extension_loaded(::Val{:Functors}) = true
 
-LuxCore.Internal.isleaf(x) = Functors.isleaf(x)
-LuxCore.Internal.fmap(args...; kwargs...) = Functors.fmap(args...; kwargs...)
-LuxCore.Internal.fleaves(args...; kwargs...) = Functors.fleaves(args...; kwargs...)
+LuxCore.Internal.isleaf_impl(x) = Functors.isleaf(x)
+LuxCore.Internal.fmap_impl(args...; kwargs...) = Functors.fmap(args...; kwargs...)
+LuxCore.Internal.fleaves_impl(args...; kwargs...) = Functors.fleaves(args...; kwargs...)
 
 function Functors.functor(::Type{<:LuxCore.AbstractLuxContainerLayer{layers}},
         x) where {layers}
-    _children = NamedTuple{layers}(getproperty.((x,), layers))
+    children = NamedTuple{layers}(getproperty.((x,), layers))
     layer_reconstructor = let x = x, layers = layers
         z -> reduce(LuxCore._setfield, zip(layers, z); init=x)
     end
-    return _children, layer_reconstructor
+    return children, layer_reconstructor
 end
 
 function Functors.functor(::Type{<:LuxCore.AbstractLuxWrapperLayer{layer}},
         x) where {layer}
-    _children = NamedTuple{(layer,)}((getproperty(x, layer),))
+    children = NamedTuple{(layer,)}((getproperty(x, layer),))
     layer_reconstructor = let x = x, layer = layer
         z -> LuxCore._setfield(x, layer, getproperty(z, layer))
     end
-    return _children, layer_reconstructor
+    return children, layer_reconstructor
 end
 
 end
