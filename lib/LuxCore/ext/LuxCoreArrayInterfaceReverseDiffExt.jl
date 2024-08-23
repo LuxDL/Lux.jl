72071ea147ef3f75347a6fe314215873cedbdb02
Avik Pal <avikpal@mit.edu>
2024-08-23 15:10:46 -0700

    chore: fix formatting

--- lib/LuxCore/ext/LuxCoreArrayInterfaceReverseDiffExt.jl
+++ lib/LuxCore/ext/LuxCoreArrayInterfaceReverseDiffExt.jl
@@ -9,8 +9,7 @@ function LuxCore.apply(
         m::AbstractLuxLayer, x::AbstractArray{<:TrackedReal}, ps, st)
     @warn "Lux.apply(m::AbstractLuxLayer, \
            x::AbstractArray{<:ReverseDiff.TrackedReal}, ps, st) input was corrected to \
-           Lux.apply(m::AbstractLuxLayer, x::ReverseDiff.TrackedArray}, ps, \
-           st).\n\n\
+           Lux.apply(m::AbstractLuxLayer, x::ReverseDiff.TrackedArray}, ps, st).\n\n\
         1. If this was not the desired behavior overload the dispatch on `m`.\n\n\
         2. This might have performance implications. Check which layer was causing this \
            problem using `Lux.Experimental.@debug_mode`." maxlog=1
