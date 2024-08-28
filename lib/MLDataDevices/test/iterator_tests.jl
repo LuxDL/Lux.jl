5d997f3b8d80f4a107d9287a0d77fd2af24649b8
Avik Pal <avikpal@mit.edu>
2024-08-28 17:52:26 -0400

    test: DataLoader aggressive freeing

--- lib/MLDataDevices/test/iterator_tests.jl
+++ lib/MLDataDevices/test/iterator_tests.jl
@@ -50,4 +50,57 @@ end
             @test get_device_type(data) == dev_type
         end
     end
+
+    @testset "DataLoader: parallel=$parallel" for parallel in (true, false)
+        X = rand(Float64, 3, 33)
+        pre = DataLoader(dev(X); batchsize=13, shuffle=false)
+        post = DataLoader(X; batchsize=13, shuffle=false) |> dev
+
+        for epoch in 1:2
+            prev_pre, prev_post = nothing, nothing
+            for (p, q) in zip(pre, post)
+                @test get_device_type(p) == dev_type
+                @test get_device_type(q) == dev_type
+                @test p ≈ q
+
+                dev_type === CPUDevice && continue
+
+                prev_pre === nothing || @test !freed_if_can_be_freed(prev_pre)
+                prev_pre = p
+
+                prev_post === nothing || @test freed_if_can_be_freed(prev_post)
+                prev_post = q
+            end
+        end
+
+        Y = rand(Float64, 1, 33)
+        pre = DataLoader((; x=dev(X), y=dev(Y)); batchsize=13, shuffle=false)
+        post = DataLoader((; x=X, y=Y); batchsize=13, shuffle=false) |> dev
+
+        for epoch in 1:2
+            prev_pre, prev_post = nothing, nothing
+            for (p, q) in zip(pre, post)
+                @test get_device_type(p.x) == dev_type
+                @test get_device_type(p.y) == dev_type
+                @test get_device_type(q.x) == dev_type
+                @test get_device_type(q.y) == dev_type
+                @test p.x ≈ q.x
+                @test p.y ≈ q.y
+
+                dev_type === CPUDevice && continue
+
+                if prev_pre !== nothing
+                    @test !freed_if_can_be_freed(prev_pre.x)
+                    @test !freed_if_can_be_freed(prev_pre.y)
+                end
+                prev_pre = p
+
+                if prev_post !== nothing
+                    @test freed_if_can_be_freed(prev_post.x)
+                    @test freed_if_can_be_freed(prev_post.y)
+                end
+                prev_post = q
+            end
+        end
+    end
 end
