9b0a3b021826f4975d7cac3683141e4d41b11641
Avik Pal <avikpal@mit.edu>
2024-10-05 16:54:15 -0400

    feat: support regression losses + tests

--- ext/LuxReactantExt/LuxReactantExt.jl
+++ ext/LuxReactantExt/LuxReactantExt.jl
@@ -1,12 +1,13 @@
 module LuxReactantExt
 
 using Enzyme: Enzyme, Const, Duplicated, Active
+using LossFunctions: LossFunctions
 using Optimisers: Optimisers
 using Reactant: Reactant, @compile, TracedRArray
 using Setfield: @set!
 using Static: False
 
-using Lux: Lux, LuxOps, Training
+using Lux: Lux, LuxOps, Training, LossFunctionImpl
 using Lux.Training: TrainingBackendCache, ReactantBackend
 
 include("overrides.jl")
--- test/reactant/loss_tests.jl
+++ test/reactant/loss_tests.jl
@@ -1,5 +1,5 @@
 @testitem "Compiled Loss Functions" tags=[:reactant] setup=[SharedTestSetup] begin
-    using Reactant, Lux
+    using Reactant, Enzyme, Lux
 
     rng = StableRNG(123)
 
@@ -31,7 +31,40 @@
             @test fn2(x, y) ≈ fn2_compiled(x_ra, y_ra)
         end
 
-        @testset "Regression Loss" begin end
+        @testset "Regression Loss" begin
+            y = [1.0, 1.0, 0.0, 0.0]
+            ŷ = [0.9, 0.1, 0.1, 0.9]
+
+            y_ra = Reactant.to_rarray(y)
+            ŷ_ra = Reactant.to_rarray(ŷ)
+
+            @testset for loss in ("MSE", "MAE", "Huber")
+                loss_mean = eval(Symbol(loss * "Loss"))()
+                loss_sum = eval(Symbol(loss * "Loss"))(; agg=sum)
+                loss_sum2 = eval(Symbol(loss * "Loss"))(; agg=(args...) -> sum(args...))
+
+                loss_mean_compiled = @compile loss_mean(ŷ_ra, y_ra)
+                @test loss_mean(ŷ, y) ≈ loss_mean_compiled(ŷ_ra, y_ra)
+
+                loss_sum_compiled = @compile loss_sum(ŷ_ra, y_ra)
+                @test loss_sum(ŷ, y) ≈ loss_sum_compiled(ŷ_ra, y_ra)
+
+                loss_sum2_compiled = @compile loss_sum2(ŷ_ra, y_ra)
+                @test loss_sum2(ŷ, y) ≈ loss_sum2_compiled(ŷ_ra, y_ra)
+            end
+
+            @testset "MSLE" begin
+                y = [123.0, 456.0, 789.0]
+                ŷ = [345.0, 332.0, 789.0]
+
+                y_ra = Reactant.to_rarray(y)
+                ŷ_ra = Reactant.to_rarray(ŷ)
+
+                loss_msle = MSLELoss()
+                loss_msle_compiled = @compile loss_msle(ŷ_ra, y_ra)
+                @test loss_msle(ŷ, y) ≈ loss_msle_compiled(ŷ_ra, y_ra)
+            end
+        end
 
         @testset "Classification Loss" begin end
 
