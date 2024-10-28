d8f6c7e5afabf4c8d1571f642fd2360ab8ec9875
Avik Pal <avikpal@mit.edu>
2024-10-28 11:19:06 -0400

    fix: missing import; fixes #179 (#180)

--- lib/LuxLib/src/impl/Impl.jl
+++ lib/LuxLib/src/impl/Impl.jl
@@ -29,7 +29,8 @@ using ..Utils: Utils, NotaNumber, batchview, concrete_bias_act_output_eltype, co
                copy_drop_gradients, eltype_mismatch, expand_batchdim,
                maybe_reduce_BLAS_threads, ofeltype_array, only_derivative, remove_tracking,
                reset_BLAS_threads, run_ka_kernel, safe_eltype, safe_vec, safe_warning,
-               unsafe_known, unrolled_mapreduce, can_loopvec_args, @enzyme_alternative
+               unsafe_known, unrolled_mapreduce, can_loopvec_args, is_extension_loaded,
+               @enzyme_alternative
 using ..Traits: activation_intermediate_not_needed, activation_has_rrule, is_mutable_array,
                 fuse_cpu_activation
 using ..System: explicit_blas_loaded, use_octavian, fits_in_l1cache, fits_in_l2cache,
