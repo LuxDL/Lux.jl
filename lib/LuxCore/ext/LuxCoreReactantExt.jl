89a7083a95d7d069a6d088a962156c6a7c95f4e2
Avik Pal <avikpal@mit.edu>
2024-11-03 21:10:31 -0500

    fix: avoid tracing through Lux models (#1021)
    * fix: avoid tracing through Lux models
    
    * ci: run lux tests

---
+++ lib/LuxCore/ext/LuxCoreReactantExt.jl
@@ -0,0 +1,13 @@
+module LuxCoreReactantExt
+
+using LuxCore: AbstractLuxLayer
+using Reactant: Reactant
+
+# Avoid tracing though models since it won't contain anything useful
+function Reactant.make_tracer(
+        seen, @nospecialize(model::AbstractLuxLayer), @nospecialize(path), mode; kwargs...
+)
+    return model
+end
+
+end
