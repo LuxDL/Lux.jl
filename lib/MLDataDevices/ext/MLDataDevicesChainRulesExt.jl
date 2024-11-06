bcf9f2e703badb13afbe4380d29c89be2fa279c6
Avik Pal <avik.pal.2017@gmail.com>
2024-11-06 15:12:33 -0500

    Apply suggestions from code review

--- lib/MLDataDevices/ext/MLDataDevicesChainRulesExt.jl
+++ lib/MLDataDevices/ext/MLDataDevicesChainRulesExt.jl
@@ -6,14 +6,10 @@ using ChainRules: OneElement
 
 Adapt.adapt_storage(::CPUDevice, x::OneElement) = x
 
-for Dev in (CUDADevice, AMDGPUDevice, MetalDevice, oneAPIDevice, ReactantDevice)
+for dev in (CUDADevice, AMDGPUDevice, MetalDevice, oneAPIDevice, ReactantDevice,
+        CUDADevice{Nothing}, AMDGPUDevice{Nothing})
     # use `@eval` to avoid ambiguity with adapt_storage(::CUDADevice, ::AbstractArray)
-    @eval Adapt.adapt_storage(to::$Dev, x::OneElement) = Adapt.adapt(to, collect(x))
-end
-
-for Dev in (CUDADevice, AMDGPUDevice)
-    # use `@eval` to avoid ambiguity with adapt_storage(::CUDADevice{Nothing}, ::AbstractArray)
-    @eval Adapt.adapt_storage(to::$Dev{Nothing}, x::OneElement) = Adapt.adapt(to, collect(x))
+    @eval Adapt.adapt_storage(to::$(dev), x::OneElement) = Adapt.adapt(to, collect(x))
 end
 
 end
