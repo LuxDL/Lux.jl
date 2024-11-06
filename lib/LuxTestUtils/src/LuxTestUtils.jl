a26f902a70867847117fdddfccf4bddecaaec2a3
Avik Pal <avikpal@mit.edu>
2024-11-06 13:16:33 -0500

    test: try re-enabling enzyme testing on 0.13.14

--- lib/LuxTestUtils/src/LuxTestUtils.jl
+++ lib/LuxTestUtils/src/LuxTestUtils.jl
@@ -37,10 +37,7 @@ try
     using Enzyme: Enzyme
     __ftest(x) = x
     Enzyme.autodiff(Enzyme.Reverse, __ftest, Enzyme.Active, Enzyme.Active(2.0))
-    # XXX: Enzyme has been causing some issues lately. Let's just disable it for now.
-    #      We still have opt-in testing available for Enzyme.
-    # XXX: Lift this once Enzyme supports 1.11 properly
-    global ENZYME_TESTING_ENABLED = false # v"1.10-" ≤ VERSION < v"1.11-"
+    global ENZYME_TESTING_ENABLED = true
 catch err
     global ENZYME_TESTING_ENABLED = false
 end
