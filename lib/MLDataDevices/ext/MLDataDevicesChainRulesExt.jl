a78aa29aed839c023f6dbd4522408c27ce09eb2d
CarloLucibello <carlo.lucibello@gmail.com>
2024-11-06 17:19:37 +0100

    fix ambiguity + oneAPI

--- lib/MLDataDevices/ext/MLDataDevicesChainRulesExt.jl
+++ lib/MLDataDevices/ext/MLDataDevicesChainRulesExt.jl
@@ -1,12 +1,18 @@
 module MLDataDevicesChainRulesExt
 
 using Adapt: Adapt
+using MLDataDevices: CPUDevice, CUDADevice, AMDGPUDevice, MetalDevice,OneAPIDevice, ReactantDevice
 using ChainRules: OneElement
-using MLDataDevices: CPUDevice, CUDADevice, AMDGPUDevice, MetalDevice, ReactantDevice
 
 Adapt.adapt_storage(::CPUDevice, x::OneElement) = x
-for Dev in (CUDADevice, AMDGPUDevice, MetalDevice, ReactantDevice)
+
+for Dev in (CUDADevice, AMDGPUDevice, MetalDevice, OneAPIDevice, ReactantDevice)
     # use `@eval` to avoid ambiguity with adapt_storage(::CUDADevice, ::AbstractArray)
+    @eval Adapt.adapt_storage(to::$Dev, x::OneElement) = Adapt.adapt(to, collect(x))
+end
+
+for Dev in (CUDADevice, AMDGPUDevice)
+    # use `@eval` to avoid ambiguity with adapt_storage(::CUDADevice{Nothing}, ::AbstractArray)
     @eval Adapt.adapt_storage(to::$Dev, x::OneElement) = Adapt.adapt(to, collect(x))
 end
 
--- lib/MLDataDevices/ext/MLDataDevicesZygoteExt.jl
+++ lib/MLDataDevices/ext/MLDataDevicesZygoteExt.jl
@@ -1,14 +1,19 @@
 module MLDataDevicesZygoteExt
 
 using Adapt: Adapt
-using MLDataDevices: CPUDevice, CUDADevice, AMDGPUDevice, MetalDevice, ReactantDevice
+using MLDataDevices: CPUDevice, CUDADevice, AMDGPUDevice, MetalDevice, OneAPIDevice, ReactantDevice
 using Zygote: OneElement
 
 Adapt.adapt_storage(::CPUDevice, x::OneElement) = x
 
-for Dev in (CUDADevice, AMDGPUDevice, MetalDevice, ReactantDevice)
+for Dev in (CUDADevice, AMDGPUDevice, MetalDevice, OneAPIDevice, ReactantDevice)
     # use `@eval` to avoid ambiguity with adapt_storage(::CUDADevice, ::AbstractArray)
     @eval Adapt.adapt_storage(to::$Dev, x::OneElement) = Adapt.adapt(to, collect(x))
+end
+
+for Dev in (CUDADevice, AMDGPUDevice)
+    # use `@eval` to avoid ambiguity with adapt_storage(::CUDADevice{Nothing}, ::AbstractArray)
+    @eval Adapt.adapt_storage(to::$Dev{Nothing}, x::OneElement) = Adapt.adapt(to, collect(x))
 end
 
 end
