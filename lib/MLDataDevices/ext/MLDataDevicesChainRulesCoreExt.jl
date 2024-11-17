097fa6c8be3c6673a06c660f454a4b5152da0560
Avik Pal <avikpal@mit.edu>
2024-11-17 15:09:39 -0500

    fix: missing imports

--- lib/MLDataDevices/ext/MLDataDevicesChainRulesCoreExt.jl
+++ lib/MLDataDevices/ext/MLDataDevicesChainRulesCoreExt.jl
@@ -3,12 +3,14 @@ module MLDataDevicesChainRulesCoreExt
 using Adapt: Adapt
 using ChainRulesCore: ChainRulesCore, NoTangent, ProjectTo, @non_differentiable
 
-using MLDataDevices: AbstractDevice, UnknownDevice, get_device, get_device_type
+using MLDataDevices: AbstractDevice, UnknownDevice, get_device, get_device_type,
+                     reactant_device, cpu_device, gpu_device
 
 @non_differentiable get_device(::Any)
 @non_differentiable get_device_type(::Any)
 @non_differentiable gpu_device(::Any)
 @non_differentiable cpu_device()
+@non_differentiable reactant_device(::Any)
 
 function ChainRulesCore.rrule(::typeof(Adapt.adapt), to::AbstractDevice, x::AbstractArray)
     dev = get_device(x)
