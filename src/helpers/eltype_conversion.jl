d79b4998e2c78df0b2c3607f129f1dcd2f9fed74
Avik Pal <avikpal@mit.edu>
2024-11-24 13:54:31 -0500

    feat: add bf16 function

--- src/helpers/eltype_conversion.jl
+++ src/helpers/eltype_conversion.jl
@@ -21,14 +21,46 @@ for (fname, ftype) in zip((:f16, :f32, :f64), (Float16, Float32, Float64))
             $($fname)(m)
 
         Converts the `eltype` of `m` *floating point* values to `$($ftype)`.
-        Recurses into structs marked with `Functors.@functor`.
+        To avoid recursion into structs mark them with `Functors.@leaf`.
         """
-        $(fname)(m) = (LuxEltypeAdaptor{$ftype}())(m)
+        $(fname)(m) = (LuxEltypeAdaptor{$(ftype)}())(m)
     end
 end
 
+@static if isdefined(Core, :BFloat16)
+    bf16_docs = """
+        bf16(m)
+
+    Converts the `eltype` of `m` *floating point* values to `BFloat16`.
+    To avoid recursion into structs mark them with `Functors.@leaf`.
+
+    !!! warning
+
+        `BFloat16s.jl` needs to be loaded before using this function.
+
+    !!! tip "Support for `BFloat16`"
+
+        Most Lux operations aren't optimized for `BFloat16` yet. Instead this is meant to be
+        used together with `Reactant.@compile`.
+    """
+
+    bf16(m) = (LuxEltypeAdaptor{Core.BFloat16}())(m)
+else
+    bf16_docs = """
+        bf16(m)
+
+    !!! danger "Not Supported"
+
+        Current Julia version does not support `BFloat16`. Use julia 1.11 or newer.
+    """
+
+    bf16(_) = error("`bf16` is not supported on Julia versions 1.11+")
+end
+
+@doc (bf16_docs) bf16
+
 # Common incorrect usage
-for f in (f16, f32, f64)
+for f in (f16, f32, f64, bf16)
     warn_msg = "$(f) is not meant to be broadcasted like `$(f).(x)` or `x .|> $(f)`, \
                 and this might give unexpected results and could lead to crashes. Directly \
                 use `$(f)` as `$(f)(x)` or `x |> $(f)` instead."
