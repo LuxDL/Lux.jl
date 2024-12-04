6fb8c4f4ee643c235db3028fd4618e7a1f56679c
Avik Pal <avikpal@mit.edu>
2024-12-04 11:40:37 +0530

    fix: use the correct dispatches for device overloads

--- lib/LuxCore/ext/LuxCoreMLDataDevicesExt.jl
+++ lib/LuxCore/ext/LuxCoreMLDataDevicesExt.jl
@@ -1,15 +1,18 @@
 module LuxCoreMLDataDevicesExt
 
+using Adapt: Adapt
 using LuxCore: LuxCore
 using MLDataDevices: MLDataDevices
 
-for (dev) in (:CPU, :CUDA, :AMDGPU, :Metal, :oneAPI)
+MLDataDevices.isleaf(::LuxCore.AbstractLuxLayer) = true
+
+for (dev) in (:CPU, :CUDA, :AMDGPU, :Metal, :oneAPI, :Reactant)
     ldev = Symbol(dev, :Device)
-    @eval function (::MLDataDevices.$(ldev))(NN::LuxCore.AbstractLuxLayer)
+    @eval function Adapt.adapt_storage(::MLDataDevices.$(ldev), x::LuxCore.AbstractLuxLayer)
         @warn "Lux layers are stateless and hence don't participate in device transfers. \
                Apply this function on the parameters and states generated using \
                `LuxCore.setup`."
-        return NN
+        return x
     end
 end
 
