a3afbf87e76e8feb3fde8ce173af5b9c5de29729
Avik Pal <avikpal@mit.edu>
2024-12-15 09:02:46 +0530

    fix: remove old patches around reactant bug

--- ext/LuxReactantExt/patches.jl
+++ ext/LuxReactantExt/patches.jl
@@ -1,7 +1,1 @@
-# For some reason xlogx and xlogy with boolean inputs leads to incorrect results sometimes
-# XXX: Once https://github.com/EnzymeAD/Reactant.jl/pull/278 is merged and tagged
-LuxOps.xlogx(x::TracedRNumber{Bool}) = zero(x)
 
-function LuxOps.xlogy(x::TracedRNumber, y::TracedRNumber)
-    return invoke(LuxOps.xlogy, Tuple{Number, Number}, float(x), float(y))
-end
