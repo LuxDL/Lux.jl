3d866cbd08a8a28596d5bca2e21d4db3e8c3abe2
Vedant Puri <vedantpuri@gmail.com>
2025-01-02 19:54:09 +0530

    device(NN) should only give warning once

--- lib/LuxCore/ext/LuxCoreMLDataDevicesExt.jl
+++ lib/LuxCore/ext/LuxCoreMLDataDevicesExt.jl
@@ -9,7 +9,7 @@ MLDataDevices.isleaf(::AbstractLuxLayer) = true
 function Adapt.adapt_storage(::AbstractDevice, x::AbstractLuxLayer)
     @warn "Lux layers are stateless and hence don't participate in device transfers. \
            Apply this function on the parameters and states generated using \
-           `LuxCore.setup`."
+           `LuxCore.setup`." maxlog=1
     return x
 end
 
