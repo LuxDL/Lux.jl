831fa8fa83e3ff78357efd7e98e9d579146621a5
Avik Pal <avikpal@mit.edu>
2025-03-16 13:26:30 -0400

    chore: setup for tests and docs
    [skip ci]

--- src/layers/attention.jl
+++ src/layers/attention.jl
@@ -1,6 +1,18 @@
-@concrete struct MultiHeadAttention <: AbstractLuxContainerLayer{
-        (:q_proj, :k_proj, :v_proj, :attention_dropout, :out_proj),
-    }
+"""
+    MultiHeadAttention(dims; nheads=1, dense_kwargs=(; use_bias=False()),
+                       attention_dropout_probability=0.0f0)
+
+The multi-head dot-product attention layer used in Transformer architectures [1].
+
+
+## References
+
+[1] Vaswani et al. "Attention is all you need." Advances in Neural Information Processing
+Systems. 2017.
+"""
+@concrete struct MultiHeadAttention <: AbstractLuxContainerLayer{(
+    :q_proj, :k_proj, :v_proj, :attention_dropout, :out_proj
+),}
     nheads <: IntegerType
     q_proj <: Dense
     k_proj <: Dense
@@ -10,33 +22,33 @@
 end
 
 function parse_mha_dims(dims::IntegerType)
-    return (;
-        q_in = dims, k_in = dims, v_in = dims, qk = dims, v = dims, out = dims,
-    )
+    return (; q_in=dims, k_in=dims, v_in=dims, qk=dims, v=dims, out=dims)
 end
 
 function parse_mha_dims((in_dims, (qkv_dims, out_dims)))
     if in_dims isa IntegerType
         q_in, k_in, v_in = in_dims, in_dims, in_dims
     else
-        @assert in_dims isa NTuple{3, <:IntegerType}
+        @assert in_dims isa NTuple{3,<:IntegerType}
         q_in, k_in, v_in = in_dims
     end
 
     if qkv_dims isa IntegerType
         qk, v = qkv_dims, qkv_dims
     else
-        @assert qkv_dims isa NTuple{2, <:IntegerType}
+        @assert qkv_dims isa NTuple{2,<:IntegerType}
         qk, v = qkv_dims
     end
 
-    return (; q_in, k_in, v_in, qk, v, out = out_dims)
+    return (; q_in, k_in, v_in, qk, v, out=out_dims)
 end
 
 function MultiHeadAttention(
-        dims; nheads::IntegerType = 1,
-        dense_kwargs = (; use_bias = False()), dropout_probability = 0.0f0
-    )
+    dims;
+    nheads::IntegerType=1,
+    dense_kwargs=(; use_bias=False()),
+    attention_dropout_probability=0.0f0,
+)
     dims = parse_mha_dims(dims)
     @argcheck dims.qk % nheads == 0
     @argcheck dims.v % nheads == 0
@@ -46,7 +58,7 @@ function MultiHeadAttention(
         Dense(dims.q_in, dims.qk; dense_kwargs...),
         Dense(dims.k_in, dims.qk; dense_kwargs...),
         Dense(dims.v_in, dims.v; dense_kwargs...),
-        Dropout(dropout_probability),
+        Dropout(attention_dropout_probability),
         Dense(dims.v, dims.out; dense_kwargs...),
     )
 end
@@ -64,24 +76,26 @@ function apply_multiheadattention(mha::MultiHeadAttention, ps, st, q, kv)
     return apply_multiheadattention(mha, ps, st, q, kv, kv, nothing)
 end
 
-function apply_multiheadattention(mha::MultiHeadAttention, ps, st, q, k, v, mask = nothing)
+function apply_multiheadattention(mha::MultiHeadAttention, ps, st, q, k, v, mask=nothing)
     q, q_st = mha.q_proj(q, ps.q_proj, st.q_proj)
     k, k_st = mha.k_proj(k, ps.k_proj, st.k_proj)
     v, v_st = mha.v_proj(v, ps.v_proj, st.v_proj)
 
     dropout = StatefulLuxLayer{true}(
-        mha.attention_dropout,
-        ps.attention_dropout, st.attention_dropout
+        mha.attention_dropout, ps.attention_dropout, st.attention_dropout
     )
-    x, α = NNlib.dot_product_attention(q, k, v; mha.nheads, fdrop = dropout, mask)
+    x, α = NNlib.dot_product_attention(q, k, v; mha.nheads, fdrop=dropout, mask)
 
     x, out_st = mha.out_proj(x, ps.out_proj, st.out_proj)
 
     return (
         x,
         (;
-            q_proj = q_st, k_proj = k_st, v_proj = v_st,
-            out_proj = out_st, attention_dropout = dropout.st,
+            q_proj=q_st,
+            k_proj=k_st,
+            v_proj=v_st,
+            out_proj=out_st,
+            attention_dropout=dropout.st,
         ),
     )
 end
---
+++ test/layers/attention_tests.jl
@@ -0,0 +1,5 @@
+@testitem "MultiHeadAttention" setup = [SharedTestSetup] tags = [:core_layers] begin
+end
+
+@testitem "Reactant MultiHeadAttention" setup = [SharedTestSetup] tags = [:reactant] begin
+end
