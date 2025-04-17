fd5e8acdd9f88668a082b62cfddddd9003d63997
Avik Pal <avikpal@mit.edu>
2025-04-17 01:15:16 -0400

    feat: rely on while deadargs removal

--- ext/LuxReactantExt/layers.jl
+++ ext/LuxReactantExt/layers.jl
@@ -4,30 +4,7 @@ function (e::Lux.Embedding)(x::TracedRNumber{<:Reactant.ReactantInt}, ps, st::Na
 end
 
 # Recurrent Layers
-# TODO: Once we can eliminate dead-args in while loop we should remove this case and only
-#       use the later function for maintenance purposes.
-function (r::Lux.Recurrence{False})(x::AnyTracedRArray, ps, st::NamedTuple)
-    if r.ordering isa Lux.TimeLastIndex ||
-        (r.ordering isa Lux.BatchLastIndex && ndims(x) == 2)
-        idxs = ntuple(Returns(Colon()), ndims(x) - 1)
-        (out, carry), st = r.cell(x[idxs..., 1], ps, st)
-        @trace for i in 2:size(x, ndims(x))
-            (out, carry), st = r.cell((x[idxs..., i], carry), ps, st)
-        end
-        return out, st
-    elseif r.ordering isa Lux.BatchLastIndex
-        idxs = ntuple(Returns(Colon()), ndims(x) - 2)
-        (out, carry), st = r.cell(x[idxs..., 1, :], ps, st)
-        @trace for i in 2:size(x, ndims(x) - 1)
-            (out, carry), st = r.cell((x[idxs..., i, :], carry), ps, st)
-        end
-        return out, st
-    else
-        error("Unknown ordering: $(r.ordering)")
-    end
-end
-
-function (r::Lux.Recurrence{True})(x::AnyTracedRArray, ps, st::NamedTuple)
+function (r::Lux.Recurrence)(x::AnyTracedRArray, ps, st::NamedTuple)
     if r.ordering isa Lux.TimeLastIndex ||
         (r.ordering isa Lux.BatchLastIndex && ndims(x) == 2)
         idxs = ntuple(Returns(Colon()), ndims(x) - 1)
@@ -50,5 +27,7 @@ function (r::Lux.Recurrence{True})(x::AnyTracedRArray, ps, st::NamedTuple)
     else
         error("Unknown ordering: $(r.ordering)")
     end
+
+    r.return_sequence isa False && return (out, st)
     return (out, eachslice(sequence; dims=ndims(sequence))), st
 end
