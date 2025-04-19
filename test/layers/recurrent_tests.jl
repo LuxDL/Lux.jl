2b55d9d7730d32915ddd61b46ebff96c812e6550
Avik Pal <avikpal@mit.edu>
2025-04-19 19:33:43 -0400

    test: add 1305 as a testcase

--- test/layers/recurrent_tests.jl
+++ test/layers/recurrent_tests.jl
@@ -613,3 +613,40 @@ end
         end
     end
 end
+
+@testitem "Issue #1305" setup = [SharedTestSetup, RecurrentLayersSetup] tags = [
+    :recurrent_layers
+] begin
+    rng = StableRNG(12345)
+
+    @testset "$mode" for (mode, aType, dev, ongpu) in MODES
+        model = Chain(
+            Recurrence(LSTMCell(4 => 4); return_sequence=true),
+            WrappedFunction(x -> Tuple(x)),
+        )
+        ps, st = Lux.setup(rng, model) |> dev
+
+        x = (
+            randn(rng, Float32, 4, 10),
+            randn(rng, Float32, 4, 10),
+            randn(rng, Float32, 4, 10),
+        ) |> dev
+
+        _f = (model, x, ps, st) -> begin
+            y, _ = model(x, ps, st)
+            return sum(sum, y)
+        end
+
+        @test_gradients(
+            _f,
+            model,
+            x,
+            ps,
+            st;
+            atol=1.0e-3,
+            rtol=1.0e-3,
+            # skip_backends=[AutoEnzyme()],
+            soft_fail=[AutoFiniteDiff()]
+        )
+    end
+end
