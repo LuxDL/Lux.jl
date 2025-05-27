5b227888352079c7462948d8e5e68f35520481de
Avik Pal <avikpal@mit.edu>
2025-05-27 11:53:47 -0400

    chore: setup to use latest reactant

---
+++ lib/LuxLib/ext/LuxLibReactantExt/LuxLibReactantExt.jl
@@ -0,0 +1,10 @@
+module LuxLibReactantExt
+
+using Reactant: Reactant, Ops, AnyTracedRArray, AnyTracedRVector
+using Static: False, True
+
+using LuxLib: LuxLib, Impl, Optional, Utils
+
+include("batchnorm.jl")
+
+end
