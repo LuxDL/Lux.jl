3db677ad9a5804670023e151a6ac72ff787fb897
Avik Pal <avikpal@mit.edu>
2025-06-18 21:29:19 -0400

    fix: remove type piracy

--- ext/LuxComponentArraysExt.jl
+++ ext/LuxComponentArraysExt.jl
@@ -1,13 +1,7 @@
 module LuxComponentArraysExt
 
-using ComponentArrays: ComponentArrays, ComponentArray, FlatAxis
+using ComponentArrays: ComponentArrays, ComponentArray
 using Lux: Lux, DistributedUtils
-
-# Empty NamedTuple: Hack to avoid breaking precompilation
-function ComponentArrays.ComponentArray(data::Vector{Any}, axes::Tuple{FlatAxis})
-    length(data) == 0 && return ComponentArray(Float32[], axes)
-    return ComponentArray{Any,1,typeof(data),typeof(axes)}(data, axes)
-end
 
 # Distributed Functionality
 function DistributedUtils.synchronize!!(
