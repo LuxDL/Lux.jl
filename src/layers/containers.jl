db72468b2f938301dc811362a9ad47ce5f2e80ba
Abhro <5664668+abhro@users.noreply.github.com>
2025-06-26 13:40:30 -0400

    Fix up markdown lists in MultiHeadAttention and RepeatedLayer
    Indent code blocks to enclose them in the list.
    Place newlines and bullets to make the alphabetic ordered list into a proper Markdown list

--- src/layers/containers.jl
+++ src/layers/containers.jl
@@ -623,21 +623,21 @@ computation, however, semantically this is same as:
 
   - `input_injection = Val(false)`
 
-```julia
-res = x
-for i in 1:repeats
-    res, st = model(res, ps, st)
-end
-```
+    ```julia
+    res = x
+    for i in 1:repeats
+        res, st = model(res, ps, st)
+    end
+    ```
 
   - `input_injection = Val(true)`
 
-```julia
-res = x
-for i in 1:repeats
-    res, st = model((res, x), ps, st)
-end
-```
+    ```julia
+    res = x
+    for i in 1:repeats
+        res, st = model((res, x), ps, st)
+    end
+    ```
 
 It is expected that `repeats` will be a reasonable number below `20`, beyond that compile
 times for gradients might be unreasonably high.
