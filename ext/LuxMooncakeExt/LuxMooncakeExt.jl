a3fd143329792a092e2f35075e2b7077d66845c4
Hong Ge <3279477+yebai@users.noreply.github.com>
2025-07-14 22:11:49 +0100

    Fix https://github.com/chalk-lab/Mooncake.jl/issues/563

--- ext/LuxMooncakeExt/LuxMooncakeExt.jl
+++ ext/LuxMooncakeExt/LuxMooncakeExt.jl
@@ -1,16 +1,18 @@
 module LuxMooncakeExt
 
 using ADTypes: AutoMooncake
-using Mooncake: Mooncake, value_and_pullback!!, prepare_pullback_cache
+using Mooncake: Mooncake, value_and_pullback!!, prepare_pullback_cache, @zero_adjoint, DefaultCtx
 using Setfield: @set!
 using Static: False, True
 
-using Lux: Lux, Utils
+using Lux: Lux, Utils, LuxLib
 using Lux.Training: TrainingBackendCache, TrainState
 
 get_config(::AutoMooncake{Nothing}) = Mooncake.Config()
 get_config(backend::AutoMooncake{<:Mooncake.Config}) = backend.config
 
 include("training.jl")
+
+@zero_adjoint DefaultCtx Tuple{typeof(LuxLib.Impl.generate_dropout_mask), AbstractRNG, Any, Any, Any, Any}
 
 end
