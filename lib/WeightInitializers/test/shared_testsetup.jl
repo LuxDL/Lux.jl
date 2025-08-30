0edb01daf2a987a28f03e64881e5aa46a5224659
Avik Pal <avikpal@mit.edu>
2025-08-30 16:26:32 -0400

    test: add checks for WeightInitializers

--- lib/WeightInitializers/test/shared_testsetup.jl
+++ lib/WeightInitializers/test/shared_testsetup.jl
@@ -18,35 +18,51 @@ if BACKEND_GROUP == "all" || BACKEND_GROUP == "cpu"
 end
 if BACKEND_GROUP == "all" || BACKEND_GROUP == "cuda"
     using CUDA
-    append!(RNGS_ARRTYPES, [
-        (CUDA.default_rng(), CuArray, true, "cuda"),
-        # See https://github.com/JuliaGPU/GPUArrays.jl/issues/614
-        # The GPUArrays.RNG for CUDA is a bit fragile
-        # (GPUArrays.default_rng(CuArray), CuArray, true, "cuda"),
-    ])
+    if CUDA.functional()
+        append!(RNGS_ARRTYPES, [
+            (CUDA.default_rng(), CuArray, true, "cuda"),
+            # See https://github.com/JuliaGPU/GPUArrays.jl/issues/614
+            # The GPUArrays.RNG for CUDA is a bit fragile
+            # (GPUArrays.default_rng(CuArray), CuArray, true, "cuda"),
+        ])
+    else
+        @assert BACKEND_GROUP == "all" "Expected CUDA.functional() to be true"
+    end
 end
 if BACKEND_GROUP == "all" || BACKEND_GROUP == "amdgpu"
     using AMDGPU
-    append!(RNGS_ARRTYPES, [
-        (AMDGPU.rocrand_rng(), ROCArray, true, "amdgpu"),
-        # See https://github.com/JuliaGPU/GPUArrays.jl/issues/614
-        # The GPUArrays.RNG for AMDGPU is a bit fragile
-        # (AMDGPU.gpuarrays_rng(), ROCArray, true, "amdgpu"),
-    ])
+    if AMDGPU.functional()
+        append!(RNGS_ARRTYPES, [
+            (AMDGPU.rocrand_rng(), ROCArray, true, "amdgpu"),
+            # See https://github.com/JuliaGPU/GPUArrays.jl/issues/614
+            # The GPUArrays.RNG for AMDGPU is a bit fragile
+            # (AMDGPU.gpuarrays_rng(), ROCArray, true, "amdgpu"),
+        ])
+    else
+        @assert BACKEND_GROUP == "all" "Expected AMDGPU.functional() to be true"
+    end
 end
 if BACKEND_GROUP == "all" || BACKEND_GROUP == "metal"
     using Metal
-    push!(RNGS_ARRTYPES, (Metal.gpuarrays_rng(), MtlArray, false, "metal"))
+    if Metal.functional()
+        push!(RNGS_ARRTYPES, (Metal.gpuarrays_rng(), MtlArray, false, "metal"))
+    else
+        @assert BACKEND_GROUP == "all" "Expected Metal.functional() to be true"
+    end
 end
 if BACKEND_GROUP == "all" || BACKEND_GROUP == "oneapi"
     using oneAPI
     using oneAPI: oneL0
 
-    supports_fp64 =
-        oneL0.module_properties(first(oneAPI.devices())).fp64flags &
-        oneL0.ZE_DEVICE_MODULE_FLAG_FP64 == oneL0.ZE_DEVICE_MODULE_FLAG_FP64
+    if oneAPI.functional()
+        supports_fp64 =
+            oneL0.module_properties(first(oneAPI.devices())).fp64flags &
+            oneL0.ZE_DEVICE_MODULE_FLAG_FP64 == oneL0.ZE_DEVICE_MODULE_FLAG_FP64
 
-    push!(RNGS_ARRTYPES, (oneAPI.gpuarrays_rng(), oneArray, supports_fp64, "oneapi"))
+        push!(RNGS_ARRTYPES, (oneAPI.gpuarrays_rng(), oneArray, supports_fp64, "oneapi"))
+    else
+        @assert BACKEND_GROUP == "all" "Expected oneAPI.functional() to be true"
+    end
 end
 
 export StableRNG, RNGS_ARRTYPES, BACKEND_GROUP, GPUArrays
