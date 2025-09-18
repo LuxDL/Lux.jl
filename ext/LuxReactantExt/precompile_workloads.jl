e4c473653f547a8eb7a69b092ad3b1a6c6a0a3e8
Avik Pal <avikpal@mit.edu>
2025-09-18 19:46:25 -0400

    fix: try moving to module

--- ext/LuxReactantExt/precompile_workloads.jl
+++ ext/LuxReactantExt/precompile_workloads.jl
@@ -1,5 +1,19 @@
 using PrecompileTools: @setup_workload, @compile_workload
 
+module PrecompileWorkloads
+
+function sumabs2attnloss(model, ps, st, data)
+    (y, _), stₙ = model(data, ps, st)
+    return sum(abs2, y), stₙ, NamedTuple()
+end
+
+function sumabs2loss(model, ps, st, data)
+    y, stₙ = model(data, ps, st)
+    return sum(abs2, y), stₙ, NamedTuple()
+end
+
+end
+
 if Reactant.Reactant_jll.is_available()
     @setup_workload begin
         orig_backend = Reactant.XLA.default_backend()
@@ -27,46 +41,28 @@ if Reactant.Reactant_jll.is_available()
 
         x = rand(Float32, (28, 28, 3, 2)) |> dev
 
-        function sumabs2attnloss(model, ps, st, data)
-            (y, _), stₙ = model(data, ps, st)
-            return sum(abs2, y), stₙ, NamedTuple()
-        end
-
-        function sumabs2loss(model, ps, st, data)
-            y, stₙ = model(data, ps, st)
-            return sum(abs2, y), stₙ, NamedTuple()
-        end
-
         @compile_workload begin
             @compile mha((q, k, v), ps_mha, LuxCore.testmode(st_mha))
-            @info "compiled mha"
 
-            # Lux.Training.single_train_step(
-            #     AutoEnzyme(),
-            #     sumabs2attnloss,
-            #     (q, k, v),
-            #     Lux.Training.TrainState(mha, ps_mha, st_mha, Optimisers.Adam(0.001f0)),
-            # )
-            @info "compiled mha train step"
+            Lux.Training.single_train_step(
+                AutoEnzyme(),
+                PrecompileWorkloads.sumabs2attnloss,
+                (q, k, v),
+                Lux.Training.TrainState(mha, ps_mha, st_mha, Optimisers.Adam(0.001f0)),
+            )
 
             @compile conv_model(x, ps_conv_model, LuxCore.testmode(st_conv_model))
-            @info "compiled conv_model"
 
-            # Lux.Training.single_train_step(
-            #     AutoEnzyme(),
-            #     sumabs2loss,
-            #     x,
-            #     Lux.Training.TrainState(
-            #         conv_model, ps_conv_model, st_conv_model, Optimisers.Adam(0.001f0)
-            #     ),
-            # )
-            @info "compiled conv_model train step"
+            Lux.Training.single_train_step(
+                AutoEnzyme(),
+                PrecompileWorkloads.sumabs2loss,
+                x,
+                Lux.Training.TrainState(
+                    conv_model, ps_conv_model, st_conv_model, Optimisers.Adam(0.001f0)
+                ),
+            )
         end
 
-        @show 111
-
         Reactant.set_default_backend(orig_backend)
-
-        @show 111212
     end
 end
--- test/reactant/loss_tests.jl
+++ test/reactant/loss_tests.jl
@@ -1,225 +1,225 @@
-# @testitem "Compiled Loss Functions" tags = [:reactant] setup = [SharedTestSetup] begin
-#     using Reactant, Lux, OneHotArrays
+@testitem "Compiled Loss Functions" tags = [:reactant] setup = [SharedTestSetup] begin
+    using Reactant, Lux, OneHotArrays
 
-#     rng = StableRNG(123)
+    rng = StableRNG(123)
 
-#     @testset "$(mode)" for (mode, atype, dev, ongpu) in MODES
-#         if mode == "amdgpu"
-#             @warn "Skipping AMDGPU tests for Reactant"
-#             continue
-#         end
+    @testset "$(mode)" for (mode, atype, dev, ongpu) in MODES
+        if mode == "amdgpu"
+            @warn "Skipping AMDGPU tests for Reactant"
+            continue
+        end
 
-#         if ongpu
-#             Reactant.set_default_backend("gpu")
-#         else
-#             Reactant.set_default_backend("cpu")
-#         end
+        if ongpu
+            Reactant.set_default_backend("gpu")
+        else
+            Reactant.set_default_backend("cpu")
+        end
 
-#         @testset "xlogx & xlogy" begin
-#             x = rand(rng, 10)
-#             y = rand(rng, 10)
-#             x_ra = Reactant.to_rarray(x)
-#             y_ra = Reactant.to_rarray(y)
+        @testset "xlogx & xlogy" begin
+            x = rand(rng, 10)
+            y = rand(rng, 10)
+            x_ra = Reactant.to_rarray(x)
+            y_ra = Reactant.to_rarray(y)
 
-#             fn1(x) = LuxOps.xlogx.(x)
-#             fn2(x, y) = LuxOps.xlogy.(x, y)
+            fn1(x) = LuxOps.xlogx.(x)
+            fn2(x, y) = LuxOps.xlogy.(x, y)
 
-#             @test fn1(x) ≈ @jit(fn1(x_ra))
-#             @test fn2(x, y) ≈ @jit(fn2(x_ra, y_ra))
-#         end
+            @test fn1(x) ≈ @jit(fn1(x_ra))
+            @test fn2(x, y) ≈ @jit(fn2(x_ra, y_ra))
+        end
 
-#         @testset "Regression Loss" begin
-#             y = [1.0, 1.0, 0.0, 0.0]
-#             ŷ = [0.9, 0.1, 0.1, 0.9]
+        @testset "Regression Loss" begin
+            y = [1.0, 1.0, 0.0, 0.0]
+            ŷ = [0.9, 0.1, 0.1, 0.9]
 
-#             y_ra = Reactant.to_rarray(y)
-#             ŷ_ra = Reactant.to_rarray(ŷ)
+            y_ra = Reactant.to_rarray(y)
+            ŷ_ra = Reactant.to_rarray(ŷ)
 
-#             @testset for loss in ("MSE", "MAE", "Huber")
-#                 loss_mean = eval(Symbol(loss * "Loss"))()
-#                 loss_sum = eval(Symbol(loss * "Loss"))(; agg=sum)
-#                 loss_sum2 = eval(Symbol(loss * "Loss"))(; agg=(args...) -> sum(args...))
+            @testset for loss in ("MSE", "MAE", "Huber")
+                loss_mean = eval(Symbol(loss * "Loss"))()
+                loss_sum = eval(Symbol(loss * "Loss"))(; agg=sum)
+                loss_sum2 = eval(Symbol(loss * "Loss"))(; agg=(args...) -> sum(args...))
 
-#                 @test loss_mean(ŷ, y) ≈ @jit(loss_mean(ŷ_ra, y_ra))
-#                 @test loss_sum(ŷ, y) ≈ @jit(loss_sum(ŷ_ra, y_ra))
-#                 @test loss_sum2(ŷ, y) ≈ @jit(loss_sum2(ŷ_ra, y_ra))
-#             end
+                @test loss_mean(ŷ, y) ≈ @jit(loss_mean(ŷ_ra, y_ra))
+                @test loss_sum(ŷ, y) ≈ @jit(loss_sum(ŷ_ra, y_ra))
+                @test loss_sum2(ŷ, y) ≈ @jit(loss_sum2(ŷ_ra, y_ra))
+            end
 
-#             @testset "MSLE" begin
-#                 y = [123.0, 456.0, 789.0]
-#                 ŷ = [345.0, 332.0, 789.0]
+            @testset "MSLE" begin
+                y = [123.0, 456.0, 789.0]
+                ŷ = [345.0, 332.0, 789.0]
 
-#                 y_ra = Reactant.to_rarray(y)
-#                 ŷ_ra = Reactant.to_rarray(ŷ)
+                y_ra = Reactant.to_rarray(y)
+                ŷ_ra = Reactant.to_rarray(ŷ)
 
-#                 loss_msle = MSLELoss()
-#                 @test loss_msle(ŷ, y) ≈ @jit(loss_msle(ŷ_ra, y_ra))
-#             end
-#         end
+                loss_msle = MSLELoss()
+                @test loss_msle(ŷ, y) ≈ @jit(loss_msle(ŷ_ra, y_ra))
+            end
+        end
 
-#         @testset "Classification Loss" begin
-#             y = Array(onehotbatch([1, 1, 0, 0], 0:1))
-#             ŷ = Array([0.1 0.9; 0.9 0.1; 0.9 0.1; 0.1 0.9]')
+        @testset "Classification Loss" begin
+            y = Array(onehotbatch([1, 1, 0, 0], 0:1))
+            ŷ = Array([0.1 0.9; 0.9 0.1; 0.9 0.1; 0.1 0.9]')
 
-#             y_ra = Reactant.to_rarray(y)
-#             ŷ_ra = Reactant.to_rarray(ŷ)
+            y_ra = Reactant.to_rarray(y)
+            ŷ_ra = Reactant.to_rarray(ŷ)
 
-#             @testset "CrossEntropyLoss" begin
-#                 celoss = CrossEntropyLoss()
-#                 @test celoss(ŷ, y) ≈ @jit(celoss(ŷ_ra, y_ra))
+            @testset "CrossEntropyLoss" begin
+                celoss = CrossEntropyLoss()
+                @test celoss(ŷ, y) ≈ @jit(celoss(ŷ_ra, y_ra))
 
-#                 celoss_ls = CrossEntropyLoss(; label_smoothing=0.1)
-#                 @test celoss_ls(ŷ, y) ≈ @jit(celoss_ls(ŷ_ra, y_ra))
+                celoss_ls = CrossEntropyLoss(; label_smoothing=0.1)
+                @test celoss_ls(ŷ, y) ≈ @jit(celoss_ls(ŷ_ra, y_ra))
 
-#                 celoss_lp = CrossEntropyLoss(; logits=Val(true))
-#                 logit_celoss_lp = (ŷ, y) -> celoss_lp(log.(ŷ), y)
-#                 @test logit_celoss_lp(ŷ, y) ≈ @jit(logit_celoss_lp(ŷ_ra, y_ra))
+                celoss_lp = CrossEntropyLoss(; logits=Val(true))
+                logit_celoss_lp = (ŷ, y) -> celoss_lp(log.(ŷ), y)
+                @test logit_celoss_lp(ŷ, y) ≈ @jit(logit_celoss_lp(ŷ_ra, y_ra))
 
-#                 celoss_lp_ls = CrossEntropyLoss(; logits=Val(true), label_smoothing=0.1)
-#                 logit_celoss_lp_ls = (ŷ, y) -> celoss_lp_ls(log.(ŷ), y)
-#                 @test logit_celoss_lp_ls(ŷ, y) ≈ @jit(logit_celoss_lp_ls(ŷ_ra, y_ra))
-#             end
+                celoss_lp_ls = CrossEntropyLoss(; logits=Val(true), label_smoothing=0.1)
+                logit_celoss_lp_ls = (ŷ, y) -> celoss_lp_ls(log.(ŷ), y)
+                @test logit_celoss_lp_ls(ŷ, y) ≈ @jit(logit_celoss_lp_ls(ŷ_ra, y_ra))
+            end
 
-#             @testset "Binary CrossEntropyLoss" begin
-#                 bceloss = BinaryCrossEntropyLoss()
-#                 @test bceloss(ŷ, y) ≈ @jit(bceloss(ŷ_ra, y_ra))
+            @testset "Binary CrossEntropyLoss" begin
+                bceloss = BinaryCrossEntropyLoss()
+                @test bceloss(ŷ, y) ≈ @jit(bceloss(ŷ_ra, y_ra))
 
-#                 bceloss_ls = BinaryCrossEntropyLoss(; label_smoothing=0.1)
-#                 @test bceloss_ls(ŷ, y) ≈ @jit(bceloss_ls(ŷ_ra, y_ra))
+                bceloss_ls = BinaryCrossEntropyLoss(; label_smoothing=0.1)
+                @test bceloss_ls(ŷ, y) ≈ @jit(bceloss_ls(ŷ_ra, y_ra))
 
-#                 bceloss_lp = BinaryCrossEntropyLoss(; logits=Val(true))
-#                 logit_bceloss_lp = (ŷ, y) -> bceloss_lp(log.(ŷ), y)
-#                 @test logit_bceloss_lp(ŷ, y) ≈ @jit(logit_bceloss_lp(ŷ_ra, y_ra))
+                bceloss_lp = BinaryCrossEntropyLoss(; logits=Val(true))
+                logit_bceloss_lp = (ŷ, y) -> bceloss_lp(log.(ŷ), y)
+                @test logit_bceloss_lp(ŷ, y) ≈ @jit(logit_bceloss_lp(ŷ_ra, y_ra))
 
-#                 bceloss_lp_ls = BinaryCrossEntropyLoss(;
-#                     logits=Val(true), label_smoothing=0.1
-#                 )
-#                 logit_bceloss_lp_ls = (ŷ, y) -> bceloss_lp_ls(log.(ŷ), y)
-#                 @test logit_bceloss_lp_ls(ŷ, y) ≈ @jit(logit_bceloss_lp_ls(ŷ_ra, y_ra))
-#             end
+                bceloss_lp_ls = BinaryCrossEntropyLoss(;
+                    logits=Val(true), label_smoothing=0.1
+                )
+                logit_bceloss_lp_ls = (ŷ, y) -> bceloss_lp_ls(log.(ŷ), y)
+                @test logit_bceloss_lp_ls(ŷ, y) ≈ @jit(logit_bceloss_lp_ls(ŷ_ra, y_ra))
+            end
 
-#             @testset "BinaryFocalLoss" begin
-#                 y = [
-#                     0 1 0
-#                     1 0 1
-#                 ]
-#                 ŷ = [
-#                     0.268941 0.5 0.268941
-#                     0.731059 0.5 0.731059
-#                 ]
+            @testset "BinaryFocalLoss" begin
+                y = [
+                    0 1 0
+                    1 0 1
+                ]
+                ŷ = [
+                    0.268941 0.5 0.268941
+                    0.731059 0.5 0.731059
+                ]
 
-#                 y_ra = Reactant.to_rarray(y)
-#                 ŷ_ra = Reactant.to_rarray(ŷ)
+                y_ra = Reactant.to_rarray(y)
+                ŷ_ra = Reactant.to_rarray(ŷ)
 
-#                 bfl = BinaryFocalLoss()
-#                 @test bfl(ŷ, y) ≈ @jit(bfl(ŷ_ra, y_ra))
-#             end
+                bfl = BinaryFocalLoss()
+                @test bfl(ŷ, y) ≈ @jit(bfl(ŷ_ra, y_ra))
+            end
 
-#             @testset "FocalLoss" begin
-#                 y = [
-#                     1 0 0 0 1
-#                     0 1 0 1 0
-#                     0 0 1 0 0
-#                 ]
-#                 ŷ = Array(softmax(reshape(-7:7, 3, 5) .* 1.0f0))
+            @testset "FocalLoss" begin
+                y = [
+                    1 0 0 0 1
+                    0 1 0 1 0
+                    0 0 1 0 0
+                ]
+                ŷ = Array(softmax(reshape(-7:7, 3, 5) .* 1.0f0))
 
-#                 y_ra = Reactant.to_rarray(y)
-#                 ŷ_ra = Reactant.to_rarray(ŷ)
+                y_ra = Reactant.to_rarray(y)
+                ŷ_ra = Reactant.to_rarray(ŷ)
 
-#                 fl = FocalLoss()
-#                 @test fl(ŷ, y) ≈ @jit(fl(ŷ_ra, y_ra))
-#             end
-#         end
+                fl = FocalLoss()
+                @test fl(ŷ, y) ≈ @jit(fl(ŷ_ra, y_ra))
+            end
+        end
 
-#         @testset "Other Losses" begin
-#             @testset "KLDivergenceLoss" begin
-#                 y = [1.0 2.0 3.0]
-#                 ŷ = [4.0 5.0 6.0]
+        @testset "Other Losses" begin
+            @testset "KLDivergenceLoss" begin
+                y = [1.0 2.0 3.0]
+                ŷ = [4.0 5.0 6.0]
 
-#                 y_ra = Reactant.to_rarray(y)
-#                 ŷ_ra = Reactant.to_rarray(ŷ)
+                y_ra = Reactant.to_rarray(y)
+                ŷ_ra = Reactant.to_rarray(ŷ)
 
-#                 kldl = KLDivergenceLoss()
-#                 @test kldl(ŷ, y) ≈ @jit(kldl(ŷ_ra, y_ra))
-#             end
+                kldl = KLDivergenceLoss()
+                @test kldl(ŷ, y) ≈ @jit(kldl(ŷ_ra, y_ra))
+            end
 
-#             @testset "HingeLoss" begin
-#                 y = [1.0, 2.0, 3.0, 4.0]
-#                 ŷ = [5.0, 6.0, 7.0, 8.0]
+            @testset "HingeLoss" begin
+                y = [1.0, 2.0, 3.0, 4.0]
+                ŷ = [5.0, 6.0, 7.0, 8.0]
 
-#                 y_ra = Reactant.to_rarray(y)
-#                 ŷ_ra = Reactant.to_rarray(ŷ)
+                y_ra = Reactant.to_rarray(y)
+                ŷ_ra = Reactant.to_rarray(ŷ)
 
-#                 hl = HingeLoss()
-#                 @test hl(ŷ, y) ≈ @jit(hl(ŷ_ra, y_ra))
+                hl = HingeLoss()
+                @test hl(ŷ, y) ≈ @jit(hl(ŷ_ra, y_ra))
 
-#                 hl = HingeLoss(; agg=mean)
-#                 @test hl(ŷ, y) ≈ @jit(hl(ŷ_ra, y_ra))
-#             end
+                hl = HingeLoss(; agg=mean)
+                @test hl(ŷ, y) ≈ @jit(hl(ŷ_ra, y_ra))
+            end
 
-#             @testset "SquaredHingeLoss" begin
-#                 y = [1.0, 2.0, 3.0, 4.0]
-#                 ŷ = [5.0, 6.0, 7.0, 8.0]
+            @testset "SquaredHingeLoss" begin
+                y = [1.0, 2.0, 3.0, 4.0]
+                ŷ = [5.0, 6.0, 7.0, 8.0]
 
-#                 y_ra = Reactant.to_rarray(y)
-#                 ŷ_ra = Reactant.to_rarray(ŷ)
+                y_ra = Reactant.to_rarray(y)
+                ŷ_ra = Reactant.to_rarray(ŷ)
 
-#                 hl = SquaredHingeLoss()
-#                 @test hl(ŷ, y) ≈ @jit(hl(ŷ_ra, y_ra))
+                hl = SquaredHingeLoss()
+                @test hl(ŷ, y) ≈ @jit(hl(ŷ_ra, y_ra))
 
-#                 hl = SquaredHingeLoss(; agg=mean)
-#                 @test hl(ŷ, y) ≈ @jit(hl(ŷ_ra, y_ra))
-#             end
+                hl = SquaredHingeLoss(; agg=mean)
+                @test hl(ŷ, y) ≈ @jit(hl(ŷ_ra, y_ra))
+            end
 
-#             @testset "PoissonLoss" begin
-#                 y = [0.1, 0.2, 0.3]
-#                 ŷ = [0.4, 0.5, 0.6]
+            @testset "PoissonLoss" begin
+                y = [0.1, 0.2, 0.3]
+                ŷ = [0.4, 0.5, 0.6]
 
-#                 y_ra = Reactant.to_rarray(y)
-#                 ŷ_ra = Reactant.to_rarray(ŷ)
+                y_ra = Reactant.to_rarray(y)
+                ŷ_ra = Reactant.to_rarray(ŷ)
 
-#                 pl = PoissonLoss()
-#                 @test pl(ŷ, y) ≈ @jit(pl(ŷ_ra, y_ra))
+                pl = PoissonLoss()
+                @test pl(ŷ, y) ≈ @jit(pl(ŷ_ra, y_ra))
 
-#                 pl = PoissonLoss(; agg=mean)
-#                 @test pl(ŷ, y) ≈ @jit(pl(ŷ_ra, y_ra))
-#             end
+                pl = PoissonLoss(; agg=mean)
+                @test pl(ŷ, y) ≈ @jit(pl(ŷ_ra, y_ra))
+            end
 
-#             @testset "DiceCoeffLoss" begin
-#                 y = [1.0, 0.5, 0.3, 2.4]
-#                 ŷ = [0.0, 1.4, 0.5, 1.2]
+            @testset "DiceCoeffLoss" begin
+                y = [1.0, 0.5, 0.3, 2.4]
+                ŷ = [0.0, 1.4, 0.5, 1.2]
 
-#                 y_ra = Reactant.to_rarray(y)
-#                 ŷ_ra = Reactant.to_rarray(ŷ)
+                y_ra = Reactant.to_rarray(y)
+                ŷ_ra = Reactant.to_rarray(ŷ)
 
-#                 dl = DiceCoeffLoss()
-#                 @test dl(ŷ, y) ≈ @jit(dl(ŷ_ra, y_ra))
+                dl = DiceCoeffLoss()
+                @test dl(ŷ, y) ≈ @jit(dl(ŷ_ra, y_ra))
 
-#                 dl = DiceCoeffLoss(; agg=mean)
-#                 @test dl(ŷ, y) ≈ @jit(dl(ŷ_ra, y_ra))
-#             end
+                dl = DiceCoeffLoss(; agg=mean)
+                @test dl(ŷ, y) ≈ @jit(dl(ŷ_ra, y_ra))
+            end
 
-#             @testset "Siamese Contrastive Loss" begin
-#                 y = [
-#                     1.0 0.0
-#                     0.0 0.0
-#                     0.0 1.0
-#                 ]
-#                 ŷ = [
-#                     0.4 0.2
-#                     0.5 0.5
-#                     0.1 0.3
-#                 ]
+            @testset "Siamese Contrastive Loss" begin
+                y = [
+                    1.0 0.0
+                    0.0 0.0
+                    0.0 1.0
+                ]
+                ŷ = [
+                    0.4 0.2
+                    0.5 0.5
+                    0.1 0.3
+                ]
 
-#                 y_ra = Reactant.to_rarray(y)
-#                 ŷ_ra = Reactant.to_rarray(ŷ)
+                y_ra = Reactant.to_rarray(y)
+                ŷ_ra = Reactant.to_rarray(ŷ)
 
-#                 sl = SiameseContrastiveLoss()
-#                 @test sl(ŷ, y) ≈ @jit(sl(ŷ_ra, y_ra))
+                sl = SiameseContrastiveLoss()
+                @test sl(ŷ, y) ≈ @jit(sl(ŷ_ra, y_ra))
 
-#                 sl = SiameseContrastiveLoss(; agg=mean)
-#                 @test sl(ŷ, y) ≈ @jit(sl(ŷ_ra, y_ra))
-#             end
-#         end
-#     end
-# end
+                sl = SiameseContrastiveLoss(; agg=mean)
+                @test sl(ŷ, y) ≈ @jit(sl(ŷ_ra, y_ra))
+            end
+        end
+    end
+end
