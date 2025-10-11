4e88ed967e95777394507dc4d3cfb3ec523787d5
Avik Pal <avikpal@mit.edu>
2025-10-11 17:44:00 -0500

    refactor: remove duplicated code

--- ext/LuxEnzymeExt/autodiff.jl
+++ ext/LuxEnzymeExt/autodiff.jl
@@ -1,8 +1,6 @@
 # VJPs
 
-function AutoDiffInternalImpl.vector_jacobian_product_impl(
-    f::F, ad::AutoEnzyme, x, v, p
-) where {F}
+function _vector_jacobian_product_impl(f::F, ad::AutoEnzyme, x, v, extra_args...) where {F}
     ad = normalize_backend(False(), ad)
     @assert ADTypes.mode(ad) isa ADTypes.ReverseMode "VJPs are only supported in reverse \
                                                       mode"
@@ -12,45 +10,42 @@ function AutoDiffInternalImpl.vector_jacobian_product_impl(
         annotate_function(ad, OOPFunctionWrapper(f)),
         Duplicated(fmap(similar, v; exclude=isleaf), fmap(copy, v; exclude=isleaf)),
         Duplicated(x, dx),
-        Const(p),
+        extra_args...,
     )
     return dx
+end
+
+function AutoDiffInternalImpl.vector_jacobian_product_impl(
+    f::F, ad::AutoEnzyme, x, v, p
+) where {F}
+    return _vector_jacobian_product_impl(f, ad, x, v, Const(p))
 end
 
 function AutoDiffInternalImpl.vector_jacobian_product_impl(
     f::F, ad::AutoEnzyme, x, v
 ) where {F}
-    ad = normalize_backend(False(), ad)
-    @assert ADTypes.mode(ad) isa ADTypes.ReverseMode "VJPs are only supported in reverse \
-                                                      mode"
-    dx = fmap(copy, x; exclude=isleaf)
-    Enzyme.autodiff(
-        ad.mode,
-        annotate_function(ad, OOPFunctionWrapper(f)),
-        Duplicated(fmap(similar, v; exclude=isleaf), fmap(copy, v; exclude=isleaf)),
-        Duplicated(x, dx),
-    )
-    return dx
+    return _vector_jacobian_product_impl(f, ad, x, v)
 end
 
 # JVPs
 
-function AutoDiffInternalImpl.jacobian_vector_product_impl(
-    f::F, ad::AutoEnzyme, x, u, p
-) where {F}
+function _jacobian_vector_product_impl(f::F, ad::AutoEnzyme, x, u, extra_args...) where {F}
     ad = normalize_backend(True(), ad)
     @assert ADTypes.mode(ad) isa ADTypes.ForwardMode "JVPs are only supported in forward \
                                                       mode"
     return only(
-        Enzyme.autodiff(ad.mode, annotate_function(ad, f), Duplicated(x, u), Const(p))
+        Enzyme.autodiff(ad.mode, annotate_function(ad, f), Duplicated(x, u), extra_args...)
     )
+end
+
+function AutoDiffInternalImpl.jacobian_vector_product_impl(
+    f::F, ad::AutoEnzyme, x, u, p
+) where {F}
+    return _jacobian_vector_product_impl(f, ad, x, u, Const(p))
 end
 
 function AutoDiffInternalImpl.jacobian_vector_product_impl(
     f::F, ad::AutoEnzyme, x, u
 ) where {F}
-    ad = normalize_backend(True(), ad)
-    @assert ADTypes.mode(ad) isa ADTypes.ForwardMode "JVPs are only supported in forward \
-                                                      mode"
-    return only(Enzyme.autodiff(ad.mode, annotate_function(ad, f), Duplicated(x, u)))
+    return _jacobian_vector_product_impl(f, ad, x, u)
 end
