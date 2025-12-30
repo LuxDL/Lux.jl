9f26f5fff53fe50ff05435656fecf79ce980f6a3
Avik Pal <avikpal@mit.edu>
2025-12-30 16:33:34 -0500

    feat: make forwarddiff a weak dependency for luxlib (#1613)

--- lib/LuxLib/ext/LuxLibCUDAExt/LuxLibCUDAExt.jl
+++ lib/LuxLib/ext/LuxLibCUDAExt/LuxLibCUDAExt.jl
@@ -1,22 +1,11 @@
 module LuxLibCUDAExt
 
-using CUDA: CUDA, CUBLAS, CuArray, CuMatrix, CuVector, CuPtr
-using ForwardDiff: ForwardDiff
+using CUDA: CUDA, CUBLAS, CuMatrix, CuVector, CuPtr
 using LinearAlgebra: LinearAlgebra, Transpose, Adjoint
-using LuxLib: LuxLib, Impl, Optional
+using LuxLib: LuxLib, Optional
 using LuxLib.Utils: ofeltype_array
 using NNlib: NNlib
 using Static: True, False
-
-# Hacky Type Piracy for ForwardDiff rules
-for op in (:logsoftmax, :softmax)
-    dual_op = Symbol(op, :_dual)
-    @eval function NNlib.$(op)(
-        x::CuArray{<:ForwardDiff.Dual{Tag,T,P}}; dims=1
-    ) where {Tag,T,P}
-        return Impl.$(dual_op)(x; dims)
-    end
-end
 
 # Low level functions
 include("cublaslt.jl")
