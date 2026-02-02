dff3f8c275e05daba0702bdeb8dff98cf4a64c74
Avik Pal <avikpal@mit.edu>
2026-02-02 13:31:38 -0600

    fix: mooncake lux

--- lib/LuxLib/test/others/onehotarray_tests.jl
+++ lib/LuxLib/test/others/onehotarray_tests.jl
@@ -1,17 +1,11 @@
-using OneHotArrays, LuxLib, Test, BenchmarkTools
+using OneHotArrays, LuxLib, Test
 
 @testset "Specialized OneHotArrays Dispatch" begin
     x = onehotbatch("aabc", "abcdefghijklmnopqrstuv")
     weight = reshape(collect(Float32, 1:(1024 * 22)), 1024, 22)
 
-    dense_res_time = @belapsed fused_dense_bias_activation(
-        identity, $(weight), $(Array(x)), nothing
-    )
-    onehot_res_time = @belapsed fused_dense_bias_activation(
-        identity, $(weight), $(x), nothing
-    )
-
-    @test onehot_res_time < dense_res_time / 5
     @test fused_dense_bias_activation(identity, weight, x, nothing) ≈
         fused_dense_bias_activation(identity, weight, Array(x), nothing)
+
+    @test LuxLib.Utils.force_3arg_mul!_dispatch(weight, weight, x)
 end
