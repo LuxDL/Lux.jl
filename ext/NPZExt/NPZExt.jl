103563015ea62c0b8c103844ecadfb78c9b320d4
Milan Kloewer <milankloewer@gmx.de>
2026-02-03 15:38:19 +0000

    Lux.load structure + types

--- ext/NPZExt/NPZExt.jl
+++ ext/NPZExt/NPZExt.jl
@@ -2,17 +2,56 @@ module NPZExt
 
 import Lux
 import NPZ
+import Random
 
 abstract type DataLoader end
 abstract type PytorchLoader <: DataLoader end
 
+struct PTLoader <: PytorchLoader end
+@kwdef struct NPZLoader <: PytorchLoader
+    dummy_field::Bool = true
+end
 
-function Lux.load(::PytorchLoader, filepath::String)
-    # load and interpret torch model here throw error/warning if format can't be interpreted
-    # then return model, parameters, states to be used in Lux
-    model = nothing
-    parameters = nothing
-    states = nothing
+# fallback
+Lux.load(::DataLoader, args...) = error("DataLoader $(typeof(args[1])) not implemented.")
+
+function Lux.load(N::NPZLoader, filepath::String)
+
+    file = NPZ.npzread(filepath)
+
+    # extract model architecture info from file
+    # TODO don't hardcode
+    layer_map = [
+        "embed_layer"  => :layer_1,
+        "layer_1"      => :layer_2,
+        "layer_2"      => :layer_4,
+        "layer_3"      => :layer_6,
+        "output_layer" => :layer_7
+    ]
+
+    # construct Lux model architecture from layer map
+    # TODO don't hardcode
+    model = Chain(
+        Dense(13 => 32, leakyrelu),
+        Dense(32 => 64, leakyrelu),
+        Dropout(0.2),
+        Dense(64 => 64, leakyrelu),
+        Dropout(0.1),
+        Dense(64 => 32, leakyrelu),
+        Dense(32 => 1)
+    )
+
+    # initialize model parameters, TODO zero initializer?
+    parameters, states = Lux.setup(Random.default_rng(), model)
+
+    # load weights from npz file into Lux model parameters
+    for (py_name, lux_sym) in layer_map
+        lux_layer_params = getproperty(parameters, lux_sym)
+        
+        lux_layer_params.weight .= Float32.(weights[py_name * ".weight"])
+        lux_layer_params.bias   .= Float32.(weights[py_name * ".bias"])
+    end
+
     return model, parameters, states
 end
 
