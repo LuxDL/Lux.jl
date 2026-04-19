30738ff1024b748fe06ec8ec4ff2936032fc1670
Avik Pal <avikpal@mit.edu>
2026-04-19 17:22:25 -0400

    fix(WeightInitializers): update GPUArrays RNG handling for newer versions

--- lib/WeightInitializers/ext/GPUArraysExt.jl
+++ lib/WeightInitializers/ext/GPUArraysExt.jl
@@ -3,8 +3,10 @@ module GPUArraysExt
 using GPUArrays: RNG
 using WeightInitializers: DeviceAgnostic
 
-function DeviceAgnostic.get_backend_array(rng::RNG, ::Type{T}, dims::Integer...) where {T}
-    return similar(rng.state, T, dims...)
+function DeviceAgnostic.get_backend_array(
+    ::RNG{AT}, ::Type{T}, dims::Integer...
+) where {AT,T}
+    return similar(AT{T,length(dims)}, dims...)
 end
 
 end
