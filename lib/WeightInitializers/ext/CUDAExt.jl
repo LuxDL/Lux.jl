8ab84bcd60bada74b85d7252b86672f978c3ead7
Avik Pal <avikpal@mit.edu>
2026-05-04 20:50:04 -0400

    fix: CUDAExt for WeightInitializers
    fixes #1709

--- lib/WeightInitializers/ext/CUDAExt.jl
+++ lib/WeightInitializers/ext/CUDAExt.jl
@@ -1,10 +1,16 @@
 module CUDAExt
 
-using CUDA: CUDA, CURAND, CuArray
+using CUDA: CUDA, CuArray
 using WeightInitializers: DeviceAgnostic
 
+const CUDA_RNG_TYPE = @static if isdefined(CUDA, :CURAND) && isdefined(CUDA.CURAND, :RNG)
+    Union{CUDA.RNG,CUDA.CURAND.RNG}
+else
+    CUDA.RNG
+end
+
 function DeviceAgnostic.get_backend_array(
-    ::Union{CUDA.RNG,CURAND.RNG}, ::Type{T}, dims::Integer...
+    ::CUDA_RNG_TYPE, ::Type{T}, dims::Integer...
 ) where {T}
     return CuArray{T}(undef, dims...)
 end
