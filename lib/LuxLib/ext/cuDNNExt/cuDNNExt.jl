a28e319172232a2703d80f4dadd4da6c7bafe1ec
Tim Besard <tim.besard@gmail.com>
2026-07-10 08:27:55 +0200

    LuxLib: fall back when cuDNN graphs are unsupported

--- lib/LuxLib/ext/cuDNNExt/cuDNNExt.jl
+++ lib/LuxLib/ext/cuDNNExt/cuDNNExt.jl
@@ -4,7 +4,9 @@ using LuxLib: LuxLib, Optional, ∂∅, Impl
 using LuxLib.Utils: Utils, safe_reshape, safe_vec, unsafe_known, recursive_unthunk
 using CUDA: CUDA, CuArray, CuVector, DenseCuArray, DenseCuVector
 using ChainRulesCore: ChainRulesCore
-using cuDNN: batchnorm_gradient!, batchnorm_inference!, batchnorm_training!
+using cuDNN: batchnorm_gradient!, batchnorm_gradient_supported,
+             batchnorm_inference!, batchnorm_inference_supported,
+             batchnorm_training!, batchnorm_training_supported
 using FastClosures: @closure
 using Static: StaticBool, False, True
 
@@ -12,7 +14,39 @@ const CRC = ChainRulesCore
 
 const cuDNNFloat = Union{Float32,Float64}
 
+const OptionalVector = Union{Nothing,AbstractVector}
+const BatchNormFallbackSignature =
+    Tuple{AbstractArray,OptionalVector,OptionalVector,OptionalVector,OptionalVector,
+          StaticBool,Any,Any,Any}
+
+function batchnorm_fallback(x, γ, β, rμ, rσ², training, σ, momentum, epsilon)
+    return invoke(Impl.batchnorm, BatchNormFallbackSignature, x, γ, β, rμ, rσ²,
+                  training, σ, momentum, epsilon)
+end
+
 include("batchnorm.jl")
+
+function batchnorm_cudnn_supported(γ, β, x, rμ, rσ², training, momentum, epsilon)
+    γ === nothing && return false
+    β === nothing && return false
+    if ndims(x) == 2
+        x = reshape(x, 1, 1, size(x, 1), size(x, 2))
+    end
+    dims = wsize(x, True())
+    γ = reshape(γ, dims)
+    β = reshape(β, dims)
+    rμ = safe_reshape(rμ, dims...)
+    rσ² = safe_reshape(rσ², dims...)
+    if unsafe_known(training)
+        batchnorm_training_supported(x, x, γ, β; running_mean=rμ,
+                                     running_var=rσ², momentum,
+                                     epsilon) || return false
+        return batchnorm_gradient_supported(x, γ, β, x, x, γ, γ, γ)
+    end
+    return batchnorm_inference_supported(x, x, γ, β, rμ, rσ²)
+end
+
+CRC.@non_differentiable batchnorm_cudnn_supported(::Any...)
 
 # api/batchnorm.jl
 function Impl.batchnorm(
@@ -27,8 +61,11 @@ function Impl.batchnorm(
     ϵ,
 ) where {T<:cuDNNFloat,F}
     rμₙ, rσ²ₙ = Impl.get_batchnorm_statistics(x, rμ, rσ², training)
-    y = Impl.batchnorm_cudnn(γ, β, x, rμₙ, rσ²ₙ, m, ϵ, training)[1]
-    return Impl.activation!!(σ, y), safe_vec(rμₙ), safe_vec(rσ²ₙ)
+    if batchnorm_cudnn_supported(γ, β, x, rμₙ, rσ²ₙ, training, m, ϵ)
+        y = Impl.batchnorm_cudnn(γ, β, x, rμₙ, rσ²ₙ, m, ϵ, training)[1]
+        return Impl.activation!!(σ, y), safe_vec(rμₙ), safe_vec(rσ²ₙ)
+    end
+    return batchnorm_fallback(x, γ, β, rμ, rσ², training, σ, m, ϵ)
 end
 
 function CRC.rrule(
